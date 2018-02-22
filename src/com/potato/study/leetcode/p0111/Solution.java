f219597fcf389027ac0be0d0d7090bb96f984af1
potatobeancox <403544651@qq.com>
2018-02-23 02:19:36 +0800

    add 111. Minimum Depth of Binary Tree

---
+++ src/com/potato/study/leetcode/p0111/Solution.java
@@ -0,0 +1,48 @@
+package com.potato.study.leetcode.p0111;
+
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 
+ * 111. Minimum Depth of Binary Tree
+ *         Given a binary tree, find its minimum depth.
+ * 
+ *         The minimum depth is the number of nodes along the shortest path from
+ *         the root node down to the nearest leaf node.
+ *
+ *         思路： 找出一刻二叉树的最短路径
+ *         递归 最小路径 是左子树右子树中最小的那个
+ * 
+ * 
+ */
+public class Solution {
+
+	
+	public int minDepth(TreeNode root) {
+        if(root == null) {
+        	return 0;
+        }
+        if(root.left != null && root.right != null) {
+        	int leftHeight = minDepth(root.left);
+            int rightHeight = minDepth(root.right);
+            return (leftHeight > rightHeight ? rightHeight :leftHeight) + 1;
+        } else if(root.left == null && root.right == null) {
+        	return 1;
+        } else if (root.left != null) {
+        	return minDepth(root.left) + 1;
+        } else { // && root.right != null
+        	return minDepth(root.right) + 1;
+        }
+        
+    }
+	
+	
+	public static void main(String[] args) {
+//		Solution solution = new Solution();
+		
+	}
+}
