58d32a8d428f99657ab0d993cf7122147906a0e9
potatobeancox <403544651@qq.com>
2018-05-10 19:13:35 +0800

    add 455. Assign Cookies

---
+++ src/com/potato/study/leetcode/p0455/Solution.java
@@ -0,0 +1,80 @@
+package com.potato.study.leetcode.p0455;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ *   455. Assign Cookiesrn
+ * 
+ *      Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie. Each child i has a greed factor gi, which is the minimum size of a cookie that the child will be content with; and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+Note:
+You may assume the greed factor is always positive. 
+You cannot assign more than one cookie to one child.
+
+Example 1:
+Input: [1,2,3], [1,1]
+
+Output: 1
+
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+You need to output 1.
+Example 2:
+Input: [1,2], [1,2,3]
+
+Output: 2
+
+Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2. 
+You have 3 cookies and their sizes are big enough to gratify all of the children, 
+You need to output 2.
+ * 
+ *         思路： 
+ *          给每个孩子分小饼干 每个孩子只能分一块 如果达到预期 他们会很满意 求最大的满意数
+ *          
+ *          排序 greed 和 饼干数  开始遍历 两个数组  i j
+ *          while i < g.len j < s.len 
+	 *          若 g i  《=     s j 满意 ++ i++ j ++
+	 *          若gi 》 sj  j++；
+ *          
+ *          
+ * 			
+ * 				
+ */	
+public class Solution {
+	
+	public int findContentChildren(int[] g, int[] s) {
+		if(null == g || null == s) {
+			return 0;
+		}
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        int count = 0;//满意的人数
+        while(i < g.length && j < s.length) {
+        	if(g[i] <= s[j]) {
+        		count++;
+        		i++;
+        		j++;
+        	} else { // 不满意
+        		j++;
+        	}
+        }
+        return count;
+    }
+	
+	
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+//		int[] g = {1,2,3};
+//		int[] s = {1,1};
+		int[] g = {1,2};
+		int[] s = {1,2,3};
+		int count = solution.findContentChildren(g, s);
+		System.out.println(count);
+		
+	}
+}
