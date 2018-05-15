88b4dfa1d2b1f4ae0c490ce5f046ec838aa02dd5
potatobeancox <403544651@qq.com>
2018-05-15 19:05:26 +0800

    add 237. Delete Node in a Linked List

---
+++ src/com/potato/study/leetcode/p0237/Solution.java
@@ -0,0 +1,48 @@
+package com.potato.study.leetcode.p0237;
+
+import com.potato.study.leetcode.domain.ListNode;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ *      237. Delete Node in a Linked List
+ * 
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+ * 
+ * 
+ * 思路：寻找两个节点的公共祖先
+ *   这个题主要难点在题意的解读
+ *   不给链表head 只给要删除的节点node 让你删除节点 
+ *   解答就是 不删节点 只修改内容 然后删除最后一个节点（前提node不是最终节点）
+ */
+public class Solution {
+	
+	public void deleteNode(ListNode node) {
+        if(node == null) {
+        	return;
+        }
+        ListNode p = node;
+        if(node.next == null) {
+        	return;
+        }
+        ListNode q = node.next;
+        while(q != null) {
+        	p.val = q.val;
+        	if(q.next == null) {
+        		break;
+        	}
+        	p = p.next;
+        	q = q.next;
+        }
+        //删除最后一个节点
+        p.next = null;
+    }
+	
+    public static void main(String[] args) {
+//		Solution solution = new Solution();
+//		solution.deleteNode(node);
+	}
+}
