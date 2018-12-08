06268f4fb61d9140719efbc4787e210493dfb7cb
liuzhao11 <liuzhao@meituan.com>
2018-12-08 20:09:18 +0800

    p140 增加题目信息 暂时放弃

---
+++ src/com/potato/study/leetcode/p0140/todo/Solution.java
@@ -0,0 +1,73 @@
+package com.potato.study.leetcode.p0140.todo;
+
+import java.util.List;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ *        140. Word Break II
+ *         
+ *         Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input:
+s = "catsanddog"
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+Output:
+[
+"cats and dog",
+"cat sand dog"
+]
+Example 2:
+
+Input:
+s = "pineapplepenapple"
+wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+Output:
+[
+"pine apple pen apple",
+"pineapple pen apple",
+"pine applepen apple"
+]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input:
+s = "catsandog"
+wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output:
+[]
+  
+ *         思路：DP+DFS 暂时看不懂
+ *
+ *
+ *
+ */
+public class Solution {
+
+	public List<String> wordBreak(String s, List<String> wordDict) {
+
+		return null;
+	}
+	
+
+
+
+	
+	public static void main(String[] args) {
+//		Solution solution = new Solution();
+////		String s = "leetcode";
+////		String words = "[\"leet\",\"code\"]";
+//		String s = "applepenapple";
+//		String words = "[\"apple\", \"pen\"]";
+//		List<String> wordDict = ListUtil.stringToList(words);
+//		boolean res = solution.wordBreak(s, wordDict);
+//		System.out.println(res);
+	}
+}
