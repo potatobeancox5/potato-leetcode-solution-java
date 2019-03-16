6e242e7cabd697ce6f3c82ef80500197b63569f0
liuzhao11 <liuzhao11@sankuai.com>
2019-03-16 16:34:13 +0800

    add 209. Minimum Size Subarray Sum for 20190126-3

--- src/main/java/com/potato/study/leetcode/p0209/Solution.java
+++ src/main/java/com/potato/study/leetcode/p0209/Solution.java
@@ -18,9 +18,52 @@ If you have figured out the O(n) solution, try coding another solution of which 
     题目需求：
 
     思路：
+        https://www.cnblogs.com/grandyang/p/4501934.html
+        左右两个指针 从0开始 异动right until和大于给的值，再移动left 直到小于s 计算最小长度
+
+
  */
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        return -1;
+        if (null == nums || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = left + 1;
+        int sum = nums[left];
+        int minLen = Integer.MAX_VALUE;
+        do {
+            // 移动right 使sum变大
+            while (sum < s && right < nums.length) {
+                sum += nums[right];
+                right++;
+            }
+            // 记录当前值
+            if (sum >= s && minLen > right - left) {
+                minLen = right - left;
+            }
+            // 移动left 是的sum 变小
+            while (sum >= s && left <= right) {
+                sum -= nums[left];
+                left++;
+                // 记录当前值
+                if (sum >= s && minLen > right - left) {
+                    minLen = right - left;
+                }
+            }
+        } while(right < nums.length);
+        if (minLen == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return minLen;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int s = 7;
+//        int[] nums = {2,3,1,2,4,3};
+        int[] nums = {};
+        int i = solution.minSubArrayLen(s, nums);
+        System.out.println(i);
     }
 }
