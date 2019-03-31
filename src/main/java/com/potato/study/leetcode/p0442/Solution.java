5e25660910f761d87aecbfabe2cc992ede16a08d
liuzhao11 <liuzhao11@meituan.com>
2019-03-31 20:57:48 +0800

    add 442. Find All Duplicates in an Array for 20190128-1

--- src/main/java/com/potato/study/leetcode/p0442/Solution.java
+++ src/main/java/com/potato/study/leetcode/p0442/Solution.java
@@ -1,5 +1,6 @@
 package com.potato.study.leetcode.p0442;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,7 +21,7 @@ Input:
 
 Output:
 [2,3]
- * 			
+ *
  *     思路：
  *      https://www.cnblogs.com/grandyang/p/6209746.html
         遍历a[i] 如果为负数 变成正数进行下面操作
@@ -31,7 +32,26 @@ Output:
  */	
 public class Solution {
     public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        if (null == nums || nums.length == 0) {
+            return result;
+        }
+        // 遍历数组 找到其值对应的 index 是否是负数 如果是负数 说明出现过
+        for (int i = 0; i < nums.length; i++) {
+            int index = (nums[i] >= 0 ? nums[i] : -1 * nums[i]) - 1;
+            if (nums[index] < 0) {
+                result.add(nums[i] >= 0 ? nums[i] : -1 * nums[i]);
+            } else {
+                nums[index] = -1 * nums[index];
+            }
+        }
+        return result;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> duplicates = solution.findDuplicates(nums);
+        System.out.println(duplicates);
     }
 }
