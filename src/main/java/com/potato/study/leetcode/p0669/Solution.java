430f971e162b34904d2ffde87bc66bbdb3bc0c61
liuzhao11 <liuzhao11@meituan.com>
2019-04-07 15:36:51 +0800

    add 669. Trim a Binary Search Tree for 20190204-3

--- src/main/java/com/potato/study/leetcode/p0669/Solution.java
+++ src/main/java/com/potato/study/leetcode/p0669/Solution.java
@@ -48,13 +48,28 @@ Output:
  *         思路：
  *          https://blog.csdn.net/hlljjy123/article/details/77895747
             递归修剪 是的所有的数字在这之间
+            递归删除
+                如果当前节点值小于 L 说明当前节点以及左子树的节点也都可以删除了 直接返回递归右子树的结果
+                如果当前节点值大于 R 说明当前节点以及右子树的节点也都可以删除了 直接返回递归左子树的结果
+                当前节点 L <= 当前节点值 <= R 说明当前节点可以留下，递归清理左子树和右子树，返回当前节点
  *
  *
  */
 public class Solution {
 
-    public TreeNode trimBST(TreeNode root, int L, int R) {
-
-        return null;
+    public TreeNode trimBST(TreeNode root, int left, int right) {
+        if (null == root) {
+            return root;
+        }
+        if (root.val < left) {
+            return trimBST(root.right, left, right);
+        }
+        if (root.val > right) {
+            return trimBST(root.left, left, right);
+        }
+        // 当前节点 L <= 当前节点值 <= R 说明当前节点可以留下，递归清理左子树和右子树，返回当前节点
+        root.left =  trimBST(root.left, left, right);
+        root.right = trimBST(root.right, left, right);
+        return root;
     }
 }
