10e72344c3b7be37e7a025153217d87a34caa4e1
liuzhao11 <liuzhao11@meituan.com>
2019-05-03 11:10:43 +0800

    add 221. Maximal Square for 20190212-1

--- src/main/java/com/potato/study/leetcode/p0221/Solution.java
+++ src/main/java/com/potato/study/leetcode/p0221/Solution.java
@@ -18,15 +18,56 @@ Input:
 1 0 0 1 0
 
 Output: 4
- * *         
- * 思路：
+ * *
  *
+ * 题目含义：
+ *  给定一个 矩阵 ，求其中1的方形最大部分的面积
+ * 思路：
  *  https://blog.csdn.net/u012501459/article/details/46553139
+ *  定义 sideLen[i][j] 为 i j 作为右下角的正方形的边长
+ *  这样可以得到状态转移方程
+ *
+ *  过程中记录最大边长
+ *  如果 ij 点为1的话
+ *      sideLen[i][j] = min {sideLen[i-1][j], sideLen[i][j-1], sideLen[i-1][j-1]} + 1
+ *  否则 sideLen[i][j] = 0;
+ *  ps 第一行第一列的边长直接为 sideLen[i][j] = ij点处的值
+ *
  */
 public class Solution {
     public int maximalSquare(char[][] matrix) {
+        // 处理异常
+        if (null == matrix || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int maxSideLen = 0;
+        int[][] sideLen = new int[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                // 第一行第一列
+                if (i == 0 || j == 0) {
+                    sideLen[i][j] = (matrix[i][j] == '1' ? 1 : 0);
+                } else if (matrix[i][j] == '1'){
+                    sideLen[i][j] = Math.min(Math.min(sideLen[i-1][j], sideLen[i][j-1]),
+                            sideLen[i-1][j-1]) + 1;
+                } else {
+                    sideLen[i][j] = 0;
+                }
+                // 设置最大值
+                if (maxSideLen < sideLen[i][j]) {
+                    maxSideLen = sideLen[i][j];
+                }
+            }
+        }
+        return maxSideLen * maxSideLen;
+    }
 
 
-        return -1;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[][] matrix = {};
+        int area = solution.maximalSquare(matrix);
+        System.out.println("area:" + area);
     }
 }
