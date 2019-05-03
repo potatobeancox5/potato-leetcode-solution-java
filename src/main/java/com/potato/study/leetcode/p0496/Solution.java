f14b304391f97aaa8ccee8769518835e46762acb
liuzhao11 <liuzhao11@meituan.com>
2019-05-03 20:35:13 +0800

    add 496. Next Greater Element I for 20190212-2

--- src/main/java/com/potato/study/leetcode/p0496/Solution.java
+++ src/main/java/com/potato/study/leetcode/p0496/Solution.java
@@ -1,5 +1,10 @@
 package com.potato.study.leetcode.p0496;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * 
  * @author liuzhao11
@@ -26,17 +31,46 @@ For number 4 in the first array, there is no next greater number for it in the s
 Note:
 All elements in nums1 and nums2 are unique.
 The length of both nums1 and nums2 would not exceed 1000.
- * 
- *         思路： 
  *
+ *
+ *         题目含义：
+ *          找到当前nums1中的节点，在nums2中对应的下一个大于它的数字的数字
+ *         思路：
             https://blog.csdn.net/TheSnowBoy_2/article/details/54923043
             用笔和纸模拟一遍吧
+            开一个栈记录之前没有找到之后最大值的数字，
+            遍历num2 当前cur
+                如果当前栈中的值 小于当前 cur 依次出栈 并记录map key 和value 当前最大值
+                如果当前栈中的值 大于 当前cur cur入栈
  * 
  */
 public class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        // 生成map对应关系
+        for (int cur : nums2) {
+            while (!stack.isEmpty() && stack.peek() < cur) {
+                map.put(stack.pop(), cur);
+            }
+            stack.add(cur);
+        }
+        // nums1 中找到对应的值 记录下来
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        int[] nums1 = {4,1,2};
+//        int[] nums2 = {1,3,4,2};
 
-        return null;
+        int[] nums1 = {2,4};
+        int[] nums2 = {1,2,3,4};
+        int[] res = solution.nextGreaterElement(nums1, nums2);
+        System.out.println(Arrays.toString(res));
     }
 }
