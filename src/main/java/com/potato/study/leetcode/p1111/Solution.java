b522956e08c96feeeee574c2df3de2a1854606be
liuzhao11 <liuzhao11@meituan.com>
2019-09-06 13:53:11 +0800

    add 1111. Maximum Nesting Depth of Two Valid Parentheses Strings for 20190328-3

--- src/main/java/com/potato/study/leetcode/p1111/Solution.java
+++ src/main/java/com/potato/study/leetcode/p1111/Solution.java
@@ -2,7 +2,6 @@ package com.potato.study.leetcode.p1111;
 
 
 import java.util.Arrays;
-import java.util.Stack;
 
 /**
  * 
@@ -45,8 +44,34 @@ Output: [0,0,0,1,1,0,1,1]
 Constraints:
 
 1 <= seq.size <= 10000
+
+
+            题目含义：
+字符串是有效的括号字符串（表示为VPS），当且仅当它由“（”和“）”字符组成时，并且：
+
+它是空字符串，或
+它可以写成AB（A与B连接），其中A和B是VPS，或者
+它可以写成（A），其中A是VPS。
+我们可以类似地定义任何VPS S的嵌套深度深度（S），如下所示：
+
+深度（“”）= 0
+深度（A + B）=最大值（深度（A），深度（B）），其中A和B是VPS
+深度（“（”+ A +“）”）= 1 +深度（A），其中A是VPS。
+例如，“”，“（）（）”和“（）（（）（））”是VPS（嵌套深度为0,1和2），“”（“和”（（）“是不是VPS的。
+
+ 真正的题目的开始：
+给定VPS seq，将其分成两个不相交的子序列A和B，使得A和B是VPS（和A.length + B.length = seq.length）。
+
+现在选择任何这样的A和B使得max（深度（A），深度（B））是最小可能值。
+
+返回一个答案数组（长度为seq.length），编码A和B的选择：如果seq [i]是A的一部分，则回答[i] = 0，否则回答[i] = 1.注意即使是多个答案可能存在，您可以退回其中任何一个。
  *         
  *         思路：
+ *          首先需要知道怎们分才能使得 AB 最小，AB 最小的条件就是AB的差值最小， 找到这个分割点
+ *          然后判断每个位置属于A或者属于B
+ *           ()(())()
+ *           https://www.acwing.com/solution/leetcode/content/2745/
+ *
  *
  *
  *
@@ -55,28 +80,41 @@ Constraints:
  */
 public class Solution {
 
+    /**
+     * 这个题真的表述的很模糊
+     * @param seq
+     * @return
+     */
     public int[] maxDepthAfterSplit(String seq) {
+        // 1. 判断 （ 深度
+        int max = Integer.MIN_VALUE;
         int[] depth = new int[seq.length()];
-        Stack<Character> chStack = new Stack<>();
+        int current = 0;
         for (int i = 0; i < seq.length(); i++) {
-            char ch = seq.charAt(i);
-            if ('(' == ch) {
-                depth[i] = chStack.size();
-                chStack.add(ch);
-            } else if (')' == ch) {
-                if (!chStack.isEmpty()) {
-                    chStack.pop();
-                }
-                depth[i] = chStack.size();
+            if ('(' == seq.charAt(i)) {
+                depth[i] = ++current;
+            } else {
+                depth[i] = current--;
+            }
+            max = Math.max(current, max);
+        }
+        // 2. 按照深度一半分割
+        max /= 2;
+        int[] res = new int[seq.length()];
+        for (int i = 0; i < seq.length(); i++) {
+            if (depth[i] <= max) {
+                res[i] = 0;
+            } else {
+                res[i] = 1;
             }
         }
-        return depth;
+        return res;
     }
 	
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-//		String seq = "(()())";
-		String seq = "()(())()";
+		String seq = "(()())";
+//		String seq = "()(())()";
         int[] ints = solution.maxDepthAfterSplit(seq);
         System.out.println(Arrays.toString(ints));
     }
