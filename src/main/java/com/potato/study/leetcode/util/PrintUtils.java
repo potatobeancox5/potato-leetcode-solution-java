8666c80dd2bb73e27e08876236b07cea289789a6
liuzhao11 <liuzhao11@meituan.com>
2019-09-18 13:31:48 +0800

    add 334. Increasing Triplet Subsequence for 20190404-1

---
+++ src/main/java/com/potato/study/leetcode/util/PrintUtils.java
@@ -0,0 +1,35 @@
+package com.potato.study.leetcode.util;
+
+/**
+ * 输出工具类，输出一些好玩的东西
+ */
+public class PrintUtils {
+    /**
+     * 输出一尊大佛
+     */
+    private static void printSuccessAndNeverBug() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("\n");
+        builder.append("                   _ooOoo_  \n");
+        builder.append("                  o8888888o\n");
+        builder.append("                  88\" . \"88\n");
+        builder.append("                  (| -_- |)\n");
+        builder.append("                  O\\  =  /O\n");
+        builder.append("               ____/`---'\\____\n");
+        builder.append("             .'  \\\\|     |//  `.\n");
+        builder.append("            /  \\\\|||  :  |||//  \\ \n");
+        builder.append("           /  _||||| -:- |||||-  \\ \n");
+        builder.append("           |   | \\\\\\  -  /// |   |\n");
+        builder.append("           | \\_|  ''\\---/''  |   |\n");
+        builder.append("           \\  .-\\__  `-`  ___/-. /\n");
+        builder.append("         ___`. .'  /--.--\\  `. . __\n");
+        builder.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
+        builder.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
+        builder.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
+        builder.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
+        builder.append("                   `=---='\n");
+        builder.append("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+        builder.append("             佛祖保佑       永无BUG\n");
+        System.out.println(builder.toString());
+    }
+}
