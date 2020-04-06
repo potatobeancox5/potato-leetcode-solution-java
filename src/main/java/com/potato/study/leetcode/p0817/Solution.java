0c0a5ac62efee2950daafd5301d32327b15770b7
liuzhao11 <liuzhao11@meituan.com>
2020-04-06 14:01:57 +0800

    add 817. Linked List Components for 20200611

---
+++ src/main/java/com/potato/study/leetcode/p0817/Solution.java
@@ -0,0 +1,77 @@
+package com.potato.study.leetcode.p0817;
+
+import com.potato.study.leetcode.domain.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	817. Linked List Components
+ *  
+ *        We are given head, the head node of a linked list containing unique integer values.
+
+We are also given the list G, a subset of the values in the linked list.
+
+Return the number of connected components in G, where two values are connected if they appear consecutively in the linked list.
+
+Example 1:
+
+Input:
+head: 0->1->2->3
+G = [0, 1, 3]
+Output: 2
+Explanation:
+0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+Example 2:
+
+Input:
+head: 0->1->2->3->4
+G = [0, 3, 1, 4]
+Output: 2
+Explanation:
+0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+Note:
+
+If N is the length of the linked list given by head, 1 <= N <= 10000.
+The value of each node in the linked list will be in the range [0, N - 1].
+1 <= G.length <= 10000.
+G is a subset of all values in the linked list.
+ *         
+ *         思路：
+ *
+ *         https://www.cnblogs.com/okokabcd/p/9284926.html
+ *
+ *
+ *         set 中的数字 位于几个片段中
+ *
+ *
+ * 
+ */
+public class Solution {
+
+    public int numComponents(ListNode head, int[] g) {
+        // 1. build set
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < g.length; i++) {
+            set.add(g[i]);
+        }
+        int part = 0;
+        if (set.contains(head.val)) {
+            part++;
+        }
+        // 2 reserve head if current is not in but next is in the set count ++
+        while (head.next != null) {
+            if (!set.contains(head.val) && set.contains(head.next.val)) {
+                part++;
+            }
+            head = head.next;
+        }
+        return part;
+    }
+
+	public static void main(String[] args) {
+    }
+}
