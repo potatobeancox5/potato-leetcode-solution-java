5b009889e75815cffb320aa905ec40dfc82b6442
liuzhao11 <liuzhao11@meituan.com>
2020-05-23 23:28:39 +0800

    add 904. Fruit Into Baskets for 20201101

---
+++ src/main/java/com/potato/study/leetcode/p0904/Solution.java
@@ -0,0 +1,103 @@
+package com.potato.study.leetcode.p0904;
+
+import org.junit.Assert;
+
+import java.util.HashMap;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	904. Fruit Into Baskets
+ *  
+ *       In a row of trees, the i-th tree produces fruit with type tree[i].
+
+You start at any tree of your choice, then repeatedly perform the following steps:
+
+Add one piece of fruit from this tree to your baskets.  If you cannot, stop.
+Move to the next tree to the right of the current tree.  If there is no tree to the right, stop.
+Note that you do not have any choice after the initial choice of starting tree: you must perform step 1, then step 2, then back to step 1, then step 2, and so on until you stop.
+
+You have two baskets, and each basket can carry any quantity of fruit, but you want each basket to only carry one type of fruit each.
+
+What is the total amount of fruit you can collect with this procedure?
+
+
+
+Example 1:
+
+Input: [1,2,1]
+Output: 3
+Explanation: We can collect [1,2,1].
+Example 2:
+
+Input: [0,1,2,2]
+Output: 3
+Explanation: We can collect [1,2,2].
+If we started at the first tree, we would only collect [0, 1].
+Example 3:
+
+Input: [1,2,3,2,2]
+Output: 4
+Explanation: We can collect [2,3,2,2].
+If we started at the first tree, we would only collect [1, 2].
+Example 4:
+
+Input: [3,3,3,1,2,1,1,2,3,3,4]
+Output: 5
+Explanation: We can collect [1,2,1,1,2].
+If we started at the first tree or the eighth tree, we would only collect 4 fruits.
+
+
+Note:
+
+1 <= tree.length <= 40000
+0 <= tree[i] < tree.length
+ *         
+ *         题目含义：
+ *
+ *
+ *         思路：
+ *             https://leetcode-cn.com/problems/fruit-into-baskets/solution/shui-guo-cheng-lan-by-leetcode/
+ *
+ *
+ */
+public class Solution {
+
+    public int totalFruit(int[] tree) {
+        int ans = 0, i = 0;
+        Counter count = new Counter();
+        for (int j = 0; j < tree.length; ++j) {
+            count.add(tree[j], 1);
+            while (count.size() >= 3) {
+                count.add(tree[i], -1);
+                if (count.get(tree[i]) == 0)
+                    count.remove(tree[i]);
+                i++;
+            }
+            ans = Math.max(ans, j - i + 1);
+        }
+        return ans;
+    }
+
+    class Counter extends HashMap<Integer, Integer> {
+        public int get(int k) {
+            return containsKey(k) ? super.get(k) : 0;
+        }
+
+        public void add(int k, int v) {
+            put(k, get(k) + v);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+		Solution solution = new Solution();
+
+        int[] tree = new int[]{1,2,1};
+        int result = solution.totalFruit(tree);
+        System.out.println(result);
+        Assert.assertEquals(3, result);
+    }
+}
