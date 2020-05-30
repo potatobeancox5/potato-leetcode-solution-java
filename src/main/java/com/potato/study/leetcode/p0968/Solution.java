7bb8bec8bdf7d131eced433b1751fc872fb50acb
liuzhao11 <liuzhao11@meituan.com>
2020-05-30 13:54:31 +0800

    add 968. Binary Tree Cameras for 20201130

--- src/main/java/com/potato/study/leetcode/p0968/Solution.java
+++ src/main/java/com/potato/study/leetcode/p0968/Solution.java
@@ -42,7 +42,7 @@ Every node has value 0.
  *          贪心算法
  *              https://www.cnblogs.com/ethanhong/p/10200550.html
  *
- *
+ *          https://leetcode-cn.com/problems/binary-tree-cameras/solution/jian-kong-er-cha-shu-by-leetcode-2/
  *
  *
  *
@@ -52,8 +52,26 @@ Every node has value 0.
 public class Solution {
 
     public int minCameraCover(TreeNode root) {
+        int[] ans = solve(root);
+        return Math.min(ans[1], ans[2]);
+    }
 
-        return -1;
+    // 0: Strict ST; All nodes below this are covered, but not this one
+    // 1: Normal ST; All nodes below and incl this are covered - no camera
+    // 2: Placed camera; All nodes below this are covered, plus camera here
+    public int[] solve(TreeNode node) {
+        if (node == null)
+            return new int[]{0, 0, 99999};
+
+        int[] left = solve(node.left);
+        int[] right = solve(node.right);
+        int mL12 = Math.min(left[1], left[2]);
+        int mR12 = Math.min(right[1], right[2]);
+
+        int d0 = left[1] + right[1];
+        int d1 = Math.min(left[2] + mR12, right[2] + mL12);
+        int d2 = 1 + Math.min(left[0], mL12) + Math.min(right[0], mR12);
+        return new int[]{d0, d1, d2};
     }
 
 
