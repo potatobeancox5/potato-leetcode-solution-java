af96f1552cf39286b5cff41f833fe91009b9de4c
liuzhao11 <liuzhao11@meituan.com>
2020-05-31 15:07:38 +0800

    add 981. Time Based Key-Value Store for 20210106

---
+++ src/main/java/com/potato/study/leetcode/p0981/TimeMap.java
@@ -0,0 +1,95 @@
+package com.potato.study.leetcode.p0981;
+
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	981. Time Based Key-Value Store
+ *  
+ *         Create a timebased key-value store class TimeMap, that supports two operations.
+
+1. set(string key, string value, int timestamp)
+
+Stores the key and value, along with the given timestamp.
+2. get(string key, int timestamp)
+
+Returns a value such that set(key, value, timestamp_prev) was called previously, with timestamp_prev <= timestamp.
+If there are multiple such values, it returns the one with the largest timestamp_prev.
+If there are no values, it returns the empty string ("").
+
+
+Example 1:
+
+Input: inputs = ["TimeMap","set","get","get","set","get","get"], inputs = [[],["foo","bar",1],["foo",1],["foo",3],["foo","bar2",4],["foo",4],["foo",5]]
+Output: [null,null,"bar","bar",null,"bar2","bar2"]
+Explanation:
+TimeMap kv;
+kv.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1
+kv.get("foo", 1);  // output "bar"
+kv.get("foo", 3); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"
+kv.set("foo", "bar2", 4);
+kv.get("foo", 4); // output "bar2"
+kv.get("foo", 5); //output "bar2"
+
+Example 2:
+
+Input: inputs = ["TimeMap","set","set","get","get","get","get","get"], inputs = [[],["love","high",10],["love","low",20],["love",5],["love",10],["love",15],["love",20],["love",25]]
+Output: [null,null,null,"","high","high","low","low"]
+
+
+Note:
+
+All key/value strings are lowercase.
+All key/value strings have length in the range [1, 100]
+The timestamps for all TimeMap.set operations are strictly increasing.
+1 <= timestamp <= 10^7
+TimeMap.set and TimeMap.get functions will be called a total of 120000 times (combined) per test case.
+ *         
+ *
+ *         思路：
+ *         https://leetcode-cn.com/problems/time-based-key-value-store/solution/ji-yu-shi-jian-de-jian-zhi-cun-chu-by-leetcode/
+ *
+ */
+public class TimeMap {
+
+    private Map<String, List<Pair<Integer, String>>> m;
+
+    public TimeMap() {
+        m = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (!m.containsKey(key)) {
+            m.put(key, new ArrayList<>());
+        }
+        m.get(key).add(new Pair(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+        if (!m.containsKey(key)) {
+            return "";
+        }
+
+        List<Pair<Integer, String>> A = m.get(key);
+        int i = Collections.binarySearch(A, new Pair<Integer, String>(timestamp, "{"),
+                (a, b) -> Integer.compare(a.getKey(), b.getKey()));
+
+        if (i >= 0) {
+
+            return A.get(i).getValue();
+        } else if (i == -1) {
+            return "";
+        } else {
+            return A.get(-i-2).getValue();
+        }
+    }
+}
