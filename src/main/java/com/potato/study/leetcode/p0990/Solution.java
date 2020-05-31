875d076b52842c12dff8e99e76476c1a1c996812
liuzhao11 <liuzhao11@meituan.com>
2020-05-31 15:21:06 +0800

    add 990. Satisfiability of Equality Equations for 20210108

---
+++ src/main/java/com/potato/study/leetcode/p0990/Solution.java
@@ -0,0 +1,98 @@
+package com.potato.study.leetcode.p0990;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	  990. Satisfiability of Equality Equations
+ *  
+ *         Given an array equations of strings that represent relationships between variables, each string equations[i] has length 4 and takes one of two different forms: "a==b" or "a!=b".  Here, a and b are lowercase letters (not necessarily different) that represent one-letter variable names.
+
+Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
+
+
+
+Example 1:
+
+Input: ["a==b","b!=a"]
+Output: false
+Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.  There is no way to assign the variables to satisfy both equations.
+Example 2:
+
+Input: ["b==a","a==b"]
+Output: true
+Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
+Example 3:
+
+Input: ["a==b","b==c","a==c"]
+Output: true
+Example 4:
+
+Input: ["a==b","b!=c","c==a"]
+Output: false
+Example 5:
+
+Input: ["c==c","b==d","x!=z"]
+Output: true
+
+
+Note:
+
+1 <= equations.length <= 500
+equations[i].length == 4
+equations[i][0] and equations[i][3] are lowercase letters
+equations[i][1] is either '=' or '!'
+equations[i][2] is '='
+ *         
+ *         思路：
+ *         https://leetcode-cn.com/problems/satisfiability-of-equality-equations/solution/java-dfs-by-qwtsc-2/
+ *
+ *
+ */
+public class Solution {
+
+
+    private List<List<Integer>> list;
+    public boolean equationsPossible(String[] equations) {
+        list = new ArrayList<>();
+        for (int i = 0; i<26; i++){
+            list.add(new ArrayList<>());
+        }
+        for (String eq : equations){
+            if (eq.charAt(1) == '='){
+                int lo = eq.charAt(0)-'a';
+                int hi = eq.charAt(3)-'a';
+                list.get(lo).add(hi);
+                list.get(hi).add(lo);
+            }
+        }
+        for (String eq : equations){
+            if (eq.charAt(1) == '!'){
+                int lo = eq.charAt(0)-'a';
+                int hi = eq.charAt(3)-'a';
+                if (judgeInTheSame(lo, hi, new boolean[list.size()])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean judgeInTheSame(int from, int to, boolean[] visited){
+        if (from==to){
+            return true;
+        }
+        visited[from] = true;
+        boolean ans = false;
+        for (int k : list.get(from)){
+            if (!visited[k]){
+                ans |= judgeInTheSame(k, to, visited);
+            }
+        }
+        return ans;
+    }
+}
