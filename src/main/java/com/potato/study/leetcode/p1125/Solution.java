35a526cb74bfe314b895e34f224ebfd26ef43843
liuzhao11 <liuzhao11@meituan.com>
2020-05-31 22:53:19 +0800

    add 1125. Smallest Sufficient Team for 20210206

---
+++ src/main/java/com/potato/study/leetcode/p1125/Solution.java
@@ -0,0 +1,176 @@
+package com.potato.study.leetcode.p1125;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	1125. Smallest Sufficient Team
+ *  
+ *         In a project, you have a list of required skills req_skills, and a list of people.  The i-th person people[i] contains a list of skills that person has.
+
+Consider a sufficient team: a set of people such that for every required skill in req_skills, there is at least one person in the team who has that skill.  We can represent these teams by the index of each person: for example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
+
+Return any sufficient team of the smallest possible size, represented by the index of each person.
+
+You may return the answer in any order.  It is guaranteed an answer exists.
+
+
+
+Example 1:
+
+Input: req_skills = ["java","nodejs","reactjs"], people = [["java"],["nodejs"],["nodejs","reactjs"]]
+Output: [0,2]
+Example 2:
+
+Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
+Output: [1,2]
+
+
+Constraints:
+
+1 <= req_skills.length <= 16
+1 <= people.length <= 60
+1 <= people[i].length, req_skills[i].length, people[i][j].length <= 16
+Elements of req_skills and people[i] are (respectively) distinct.
+req_skills[i][j], people[i][j][k] are lowercase English letters.
+Every skill in people[i] is a skill in req_skills.
+It is guaranteed a sufficient team exists.
+
+ *         
+ *         思路：
+ *         https://leetcode-cn.com/problems/smallest-sufficient-team/solution/java-hui-su-jian-zhi-wei-yun-suan-by-mxnhujryvx/
+ *
+ *
+ */
+public class Solution {
+
+    //成员技能对应二进制数字都数组下标
+    private int[] peopleSubscript;
+
+    //当前递归深度
+    private int currentDepth = 0;
+
+    public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        int[] result = null;
+        int skillLen = req_skills.length, mustSkill = (1 << skillLen) - 1, peopleSize = people.size();
+        //技能对应二进制，用1，10，100来表示
+        Map<String, Integer> skillMap = new HashMap<String, Integer>(skillLen << 1);
+        for (int i = 0; i < skillLen; i++) {
+            skillMap.put(req_skills[i], 1 << i);
+        }
+        //员工技能对应二进制数
+        int[] peopleSkillNums = new int[peopleSize];
+        peopleSubscript = new int[1 << skillLen];
+        for (int i = 0; i < people.size(); i++) {
+            int skillNum = 0;
+            List<String> skills = people.get(i);
+            for (String skill : skills) {
+                skillNum += skillMap.get(skill);
+            }
+            peopleSkillNums[i] = skillNum;
+            peopleSubscript[skillNum] = i;
+        }
+        //技能或运算结果
+        int comSkills = 0;
+        //和其他所有员工没交集对应员工数量
+        int aloneCount = 0;
+        //和其他所有员工没交集对应员工数组下标
+        int[] noIntersectionArr = new int[skillLen];
+        for (int i = 0; i < peopleSize; i++) {
+            if (peopleSkillNums[i] == 0) {
+                continue;
+            }
+            //是否无交集
+            boolean isNoIntersection = true;
+            for (int j = 0; j < peopleSize; j++) {
+                if (peopleSkillNums[j] == 0 || i == j) {
+                    continue;
+                }
+                //重复的，去重
+                if (peopleSkillNums[i] == peopleSkillNums[j]) {
+                    peopleSkillNums[j] = 0;
+                    continue;
+                }
+                //若一个员工技能是另外一个员工子集，则必定不在最优解中，去除
+                if ((peopleSkillNums[i] | peopleSkillNums[j]) == peopleSkillNums[j]) {
+                    peopleSkillNums[i] = 0;
+                    isNoIntersection = false;
+                    break;
+                } else if ((peopleSkillNums[i] | peopleSkillNums[j]) == peopleSkillNums[i]) {
+                    peopleSkillNums[j] = 0;
+                    continue;
+                }
+                if ((peopleSkillNums[i] & peopleSkillNums[j]) != 0) {
+                    isNoIntersection = false;
+                    break;
+                }
+            }
+            //无交集员工提前保存，方便之后回溯（降低后续回溯深度）
+            if (isNoIntersection) {
+                comSkills |= peopleSkillNums[i];
+                noIntersectionArr[aloneCount] = peopleSubscript[peopleSkillNums[i]];
+                peopleSkillNums[i] = 0;
+                aloneCount++;
+            }
+        }
+        //员工技能数字排序
+        Arrays.sort(peopleSkillNums);
+        //最小回溯深度，由小到大，则第一个得到结果就为最优解
+        int minDepth = aloneCount;
+        //若无交集员工技能组成等于必须技能，则输出结果，否则开始回溯深度+1
+        if (comSkills == mustSkill) {
+            result = new int[aloneCount];
+            System.arraycopy(noIntersectionArr, 0, result, 0, aloneCount);
+            return result;
+        } else {
+            minDepth++;
+        }
+        //从minDepth回溯深度开始回溯，noIntersectionArr肯定在该结果中，回溯深度从无交集员工数量开始
+        for (int i = minDepth; i < skillLen; i++) {
+            currentDepth = i;
+            result = new int[i];
+            System.arraycopy(noIntersectionArr, 0, result, 0, aloneCount);
+            if (addNextPeople(mustSkill, comSkills, result, peopleSkillNums, aloneCount)) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    private boolean addNextPeople(int mustSkill, int comSkills, int[] result, int[] peopleSkillNums, int count) {
+        //判断是否为解
+        if (mustSkill == comSkills) {
+            return true;
+        }
+        //大于回溯深度，则不存在
+        if (count >= currentDepth) {
+            return false;
+        }
+        for (int i = peopleSkillNums.length - 1; i >= 0; i--) {
+            int skillNum = peopleSkillNums[i];
+            //由于排序，则技能为0则后续都为0，直接结束
+            if (skillNum == 0) {
+                break;
+            }
+            //组合技能已包含该技能，则跳过
+            if ((comSkills | skillNum) == comSkills) {
+                continue;
+            }
+            result[count] = peopleSubscript[peopleSkillNums[i]];
+            peopleSkillNums[i] = 0;
+            if (addNextPeople(mustSkill, comSkills | skillNum, result, peopleSkillNums, count + 1)) {
+                return true;
+            }
+            peopleSkillNums[i] = skillNum;
+        }
+        return false;
+    }
+
+}
