c09f0b4c9464a40ab41bdc2b035162cf8d4d290f
liuzhao11 <liuzhao11@meituan.com>
2020-05-31 17:48:45 +0800

    add 1035. Uncrossed Lines for 20210122

---
+++ src/main/java/com/potato/study/leetcode/p1035/Solution.java
@@ -0,0 +1,68 @@
+package com.potato.study.leetcode.p1035;
+
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	1035. Uncrossed Lines
+ *  
+ *        We write the integers of A and B (in the order they are given) on two separate horizontal lines.
+
+Now, we may draw connecting lines: a straight line connecting two numbers A[i] and B[j] such that:
+
+A[i] == B[j];
+The line we draw does not intersect any other connecting (non-horizontal) line.
+Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
+
+Return the maximum number of connecting lines we can draw in this way.
+
+
+
+Example 1:
+
+
+Input: A = [1,4,2], B = [1,2,4]
+Output: 2
+Explanation: We can draw 2 uncrossed lines as in the diagram.
+We cannot draw 3 uncrossed lines, because the line from A[1]=4 to B[2]=4 will intersect the line from A[2]=2 to B[1]=2.
+Example 2:
+
+Input: A = [2,5,1,2,5], B = [10,5,2,1,5,2]
+Output: 3
+Example 3:
+
+Input: A = [1,3,7,1,7,5], B = [1,9,2,5,1]
+Output: 2
+
+
+Note:
+
+1 <= A.length <= 500
+1 <= B.length <= 500
+1 <= A[i], B[i] <= 2000
+ *         
+ *         思路：
+ *          https://leetcode-cn.com/problems/uncrossed-lines/solution/tong-zui-chang-gong-gong-zi-xu-lie-by-joseph_liu-2/
+ *
+ *
+ *
+ */
+public class Solution {
+
+    public int maxUncrossedLines(int[] aaa, int[] bb) {
+        int n1 = aaa.length;
+        int n2 = bb.length;
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(aaa[i]==bb[j]){
+                    dp[i+1][j+1] = dp[i][j]+1;
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+}
