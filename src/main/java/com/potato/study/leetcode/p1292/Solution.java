65db586228fc87708a7439edb267a7fe29f47b71
liuzhao11 <liuzhao11@meituan.com>
2020-06-06 13:17:07 +0800

    add 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold for 20210225

---
+++ src/main/java/com/potato/study/leetcode/p1292/Solution.java
@@ -0,0 +1,76 @@
+package com.potato.study.leetcode.p1292;
+
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+ *  
+ *
+Given a m x n matrix mat and an integer threshold. Return the maximum side-length of a square with a sum less than or equal to threshold or return 0 if there is no such square.
+
+
+
+Example 1:
+
+
+Input: mat = [[1,1,3,2,4,3,2],[1,1,3,2,4,3,2],[1,1,3,2,4,3,2]], threshold = 4
+Output: 2
+Explanation: The maximum side length of square with sum less than 4 is 2 as shown.
+Example 2:
+
+Input: mat = [[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2]], threshold = 1
+Output: 0
+Example 3:
+
+Input: mat = [[1,1,1,1],[1,0,0,0],[1,0,0,0],[1,0,0,0]], threshold = 6
+Output: 3
+Example 4:
+
+Input: mat = [[18,70],[61,1],[25,85],[14,40],[11,96],[97,96],[63,45]], threshold = 40184
+Output: 2
+
+
+Constraints:
+
+1 <= m, n <= 300
+m == mat.length
+n == mat[i].length
+0 <= mat[i][j] <= 10000
+0 <= threshold <= 10^5
+ *         
+ *         思路：
+ *         https://leetcode-cn.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/solution/qing-xi-tu-jie-mo-neng-de-qian-zhui-he-by-hlxing/
+ *
+ *
+
+ *
+ */
+public class Solution {
+
+    public int maxSideLength(int[][] mat, int threshold) {
+        int m = mat.length, n = mat[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = mat[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+            }
+        }
+        int ans = 0;
+        for (int k = 1; k <= Math.min(m, n); k++) {
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i - k < 0 || j - k < 0) {
+                        continue;
+                    }
+                    int tmp = dp[i][j] - dp[i - k][j] - dp[i][j - k] + dp[i - k][j - k];
+                    if (tmp <= threshold) {
+                        ans = Math.max(ans, k);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
