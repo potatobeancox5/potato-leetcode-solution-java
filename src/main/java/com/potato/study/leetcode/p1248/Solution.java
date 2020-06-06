ff421ea6d9ab06e5bc95135ff38b38e7923d9575
liuzhao11 <liuzhao11@meituan.com>
2020-06-06 09:45:36 +0800

    add 1248. Count Number of Nice Subarrays for 20210219

---
+++ src/main/java/com/potato/study/leetcode/p1248/Solution.java
@@ -0,0 +1,88 @@
+package com.potato.study.leetcode.p1248;
+
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	1248. Count Number of Nice Subarrays
+ *  
+ *      Given an array of integers nums and an integer k. A subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There is no odd numbers in the array.
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+
+
+Constraints:
+
+1 <= nums.length <= 50000
+1 <= nums[i] <= 10^5
+1 <= k <= nums.length
+ *         
+ *
+ *
+ *         思路：
+ *          https://leetcode-cn.com/problems/count-number-of-nice-subarrays/solution/hua-dong-chuang-kou-qian-zhui-he-bi-xu-miao-dong-b/
+ *
+ *
+ *
+ *
+ */
+public class Solution {
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        int left = 0, right = 0, oddCnt = 0, res = 0;
+        while (right < nums.length) {
+            // 右指针先走，每遇到一个奇数则 oddCnt++。
+            if ((nums[right++] & 1) == 1) {
+                oddCnt++;
+            }
+
+            //  若当前滑动窗口 [left, right) 中有 k 个奇数了，进入此分支统计当前滑动窗口中的优美子数组个数。
+            if (oddCnt == k) {
+                // 先将滑动窗口的右边界向右拓展，直到遇到下一个奇数（或出界）
+                // rightEvenCnt 即为第 k 个奇数右边的偶数的个数
+                int tmp = right;
+                while (right < nums.length && (nums[right] & 1) == 0) {
+                    right++;
+                }
+                int rightEvenCnt = right - tmp;
+                // leftEvenCnt 即为第 1 个奇数左边的偶数的个数
+                int leftEvenCnt = 0;
+                while ((nums[left] & 1) == 0) {
+                    leftEvenCnt++;
+                    left++;
+                }
+                // 第 1 个奇数左边的 leftEvenCnt 个偶数都可以作为优美子数组的起点
+                // (因为第1个奇数左边可以1个偶数都不取，所以起点的选择有 leftEvenCnt + 1 种）
+                // 第 k 个奇数右边的 rightEvenCnt 个偶数都可以作为优美子数组的终点
+                // (因为第k个奇数右边可以1个偶数都不取，所以终点的选择有 rightEvenCnt + 1 种）
+                // 所以该滑动窗口中，优美子数组左右起点的选择组合数为 (leftEvenCnt + 1) * (rightEvenCnt + 1)
+                res += (leftEvenCnt + 1) * (rightEvenCnt + 1);
+
+                // 此时 left 指向的是第 1 个奇数，因为该区间已经统计完了，因此 left 右移一位，oddCnt--
+                left++;
+                oddCnt--;
+            }
+
+        }
+
+        return res;
+    }
+}
