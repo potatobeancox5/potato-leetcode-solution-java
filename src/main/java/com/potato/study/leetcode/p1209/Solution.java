5ef848d81a1ab43d3d12659d770c7c01c989381a
liuzhao11 <liuzhao11@meituan.com>
2020-06-06 09:28:40 +0800

    add 1209. Remove All Adjacent Duplicates in String II for 20210219

---
+++ src/main/java/com/potato/study/leetcode/p1209/Solution.java
@@ -0,0 +1,72 @@
+package com.potato.study.leetcode.p1209;
+
+
+/**
+ * 
+ * @author liuzhao11
+ * 
+ * 	1209. Remove All Adjacent Duplicates in String II
+ *  
+ *
+Given a string s, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made.
+
+It is guaranteed that the answer is unique.
+
+
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation:
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+
+
+Constraints:
+
+1 <= s.length <= 10^5
+2 <= k <= 10^4
+s only contains lower case English letters.
+ *         
+ *         思路：
+ *
+ *  https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string-ii/solution/shan-chu-zi-fu-chuan-zhong-de-suo-you-xiang-lin--4/
+ *
+ */
+public class Solution {
+
+    public String removeDuplicates(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        int length = -1;
+        while (length != sb.length()) {
+            length = sb.length();
+            for (int i = 0, count = 1; i < sb.length(); ++i) {
+                if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                    count = 1;
+                } else if (++count == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
+
