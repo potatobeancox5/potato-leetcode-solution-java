672f173ad459c2ec0490dbd74525ac61160ec069
liuzhao11 <liuzhao11@meituan.com>
2020-10-01 10:38:23 +0800

    修改目录结构

---
+++ src/main/java/com/potato/study/leetcodecn/p0020/t001/Solution.java
@@ -0,0 +1,79 @@
+package com.potato.study.leetcodecn.p0020.t001;
+
+
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+ 有效字符串需满足：
+
+ 左括号必须用相同类型的右括号闭合。
+ 左括号必须以正确的顺序闭合。
+ 注意空字符串可被认为是有效字符串。
+
+ 示例 1:
+
+ 输入: "()"
+ 输出: true
+ 示例 2:
+
+ 输入: "()[]{}"
+ 输出: true
+ 示例 3:
+
+ 输入: "(]"
+ 输出: false
+ 示例 4:
+
+ 输入: "([)]"
+ 输出: false
+ 示例 5:
+
+ 输入: "{[]}"
+ 输出: true
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/valid-parentheses
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * 栈的经典应用场景
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            //'('，')'，'{'，'}'，'['，']'
+            if ('(' == ch || '{' == ch || '[' == ch) {
+                stack.push(ch);
+            } else if (')' == ch) {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if ('}' == ch) {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (']' == ch) {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
