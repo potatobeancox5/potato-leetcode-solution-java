0b21a3438da6a5ac07686a8714d08ef2dec9cae7
liuzhao11 <liuzhao11@meituan.com>
2021-01-03 16:05:16 +0800

    2021.1.3 升级目录-1 2021.1.4

---
+++ src/main/java/com/potato/study/leetcodecn/p00019/t001/Solution.java
@@ -0,0 +1,57 @@
+package com.potato.study.leetcodecn.p00019.t001;
+
+
+import com.potato.study.leetcode.domain.ListNode;
+
+/**
+ * 19. 删除链表的倒数第N个节点
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+ 示例：
+
+ 给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+ 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ 说明：
+
+ 给定的 n 保证是有效的。
+
+ 进阶：
+
+ 你能尝试使用一趟扫描实现吗？
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * 先往前移动n，然后开始移动指针
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode front = head;
+        for (int i = 1; i < n; i++) {
+            front = front.next;
+        }
+        // 哨兵简化
+        ListNode target = new ListNode(-1);
+        target.next = head;
+        while (front.next != null) {
+            front = front.next;
+            target = target.next;
+        }
+        // 如果删除的是头节点咋整
+        if (target.next == head) {
+            head = target.next.next;
+        }
+        // 开始删除
+        target.next = target.next.next;
+        return head;
+    }
+}
---
+++ src/main/java/com/potato/study/leetcodecn/p00035/t001/Solution.java
@@ -0,0 +1,89 @@
+package com.potato.study.leetcodecn.p00035.t001;
+
+import org.junit.Assert;
+
+/**
+ * 35. 搜索插入位置
+ *
+ * 定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+ 你可以假设数组中无重复元素。
+
+ 示例 1:
+
+ 输入: [1,3,5,6], 5
+ 输出: 2
+ 示例 2:
+
+ 输入: [1,3,5,6], 2
+ 输出: 1
+ 示例 3:
+
+ 输入: [1,3,5,6], 7
+ 输出: 4
+ 示例 4:
+
+ 输入: [1,3,5,6], 0
+ 输出: 0
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/search-insert-position
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * 找到第一个 大于等于这个数字（target）的位置
+     * 找mid  mid < target 往后找
+     * 否则 判断 是不是第一个位置
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            // 移位 优化
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                //  mid 位置 大于等于target 有可能是第一个哦
+                if (mid == 0 || nums[mid - 1] < target) {
+                    // 第一个
+                    return mid;
+                } else {
+                    // 不是第一个 往前找
+                    right = mid - 1;
+                }
+            }
+        }
+        //  理论上走不到这
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[]{1,3,5,6};
+        int target = 5;
+        int index = solution.searchInsert(nums, target);
+        System.out.println(index);
+        Assert.assertEquals(2, index);
+
+        nums = new int[]{1,3,5,6};
+        target = 2;
+        index = solution.searchInsert(nums, target);
+        System.out.println(index);
+        Assert.assertEquals(1, index);
+
+
+        nums = new int[]{1,3,5,6};
+        target = 7;
+        index = solution.searchInsert(nums, target);
+        System.out.println(index);
+        Assert.assertEquals(4, index);
+    }
+}
