6707718d29bbbd015ae6e364b284c16857aabbe5
liuzhao11 <liuzhao11@meituan.com>
2021-01-13 13:22:19 +0800

    2021.1.13 771. 宝石与石头 2021.1.14

---
+++ src/main/java/com/potato/study/leetcodecn/p00771/t001/Solution.java
@@ -0,0 +1,56 @@
+package com.potato.study.leetcodecn.p00771.t001;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 771. 宝石与石头
+ *
+ * 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+ J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+ 示例 1:
+
+ 输入: J = "aA", S = "aAAbbbb"
+ 输出: 3
+ 示例 2:
+
+ 输入: J = "z", S = "ZZ"
+ 输出: 0
+ 注意:
+
+ S 和 J 最多含有50个字母。
+  J 中的字符不重复。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/jewels-and-stones
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * set 记录宝石种类
+     * 遍历 s 计数
+     * @param j
+     * @param s
+     * @return
+     */
+    public int numJewelsInStones(String j, String s) {
+        if (null == j || null == s) {
+            return 0;
+        }
+        Set<Character> jewelSet = new HashSet<>();
+        for (char jj : j.toCharArray()) {
+            jewelSet.add(jj);
+        }
+        int count = 0;
+        for (char ss : s.toCharArray()) {
+            if (jewelSet.contains(ss)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
