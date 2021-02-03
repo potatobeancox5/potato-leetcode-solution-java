1985fdb898cc56b7ac77acfd029ed13ca2ef6419
liuzhao11 <liuzhao11@meituan.com>
2021-02-03 17:37:05 +0800

    2021.2.3 844. 比较含退格的字符串 2021.5.6

---
+++ src/main/java/com/potato/study/leetcodecn/p00821/t001/Solution.java
@@ -0,0 +1,56 @@
+package com.potato.study.leetcodecn.p00821.t001;
+
+import java.util.Arrays;
+
+/**
+ * 821. 字符的最短距离
+ *
+ * 给定一个字符串 S 和一个字符 C。返回一个代表字符串 S 中每个字符到字符串 S 中的字符 C 的最短距离的数组。
+
+  
+
+ 示例：
+
+ 输入：S = "loveleetcode", C = 'e'
+ 输出：[3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+  
+
+ 提示：
+
+ 字符串 S 的长度范围为 [1, 10000]。
+ C 是一个单字符，且保证是字符串 S 里的字符。
+ S 和 C 中的所有字母均为小写字母。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/shortest-distance-to-a-character
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     *
+     * @param s
+     * @param c
+     * @return
+     */
+    public int[] shortestToChar(String s, char c) {
+        if (null == s || "".equals(s)) {
+            return new int[]{};
+        }
+        int[] res = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            res[i] = Math.abs((int)ch - (int)c);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "loveleetcode";
+        char c = 'e';
+        int[] ints = solution.shortestToChar(s, c);
+        System.out.println(Arrays.toString(ints));
+    }
+}
