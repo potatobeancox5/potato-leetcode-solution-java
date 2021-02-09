cf6c41544e0e33ac2aac255aba8a5685b823fc6d
liuzhao11 <liuzhao11@meituan.com>
2021-02-09 10:57:34 +0800

    2021.2.8 90. 子集 II 2021.6.15

---
+++ src/main/java/com/potato/study/leetcodecn/p00414/t001/Solution.java
@@ -0,0 +1,56 @@
+package com.potato.study.leetcodecn.p00414.t001;
+
+import java.util.PriorityQueue;
+
+/**
+ * 414. 第三大的数
+ *
+ * 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+
+  
+
+ 示例 1：
+
+ 输入：[3, 2, 1]
+ 输出：1
+ 解释：第三大的数是 1 。
+ 示例 2：
+
+ 输入：[1, 2]
+ 输出：2
+ 解释：第三大的数不存在, 所以返回最大的数 2 。
+ 示例 3：
+
+ 输入：[2, 2, 3, 1]
+ 输出：1
+ 解释：注意，要求返回第三大的数，是指第三大且唯一出现的数。
+ 存在两个值为2的数，它们都排第二。
+  
+
+ 提示：
+
+ 1 <= nums.length <= 104
+ -231 <= nums[i] <= 231 - 1
+  
+
+ 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/third-maximum-number
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * 第三大的数组
+     * @param nums
+     * @return
+     */
+    public int thirdMax(int[] nums) {
+        // 小根堆
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+//        fori
+        return -1;
+    }
+}
