49d76d6d5c0025241cd6aad6fa1d2ed0bc96bd12
liuzhao11 <liuzhao11@meituan.com>
2021-02-11 09:41:27 +0800

    2021.2.11 175. 组合两个表 2021.7.7

--- src/main/java/com/potato/study/leetcodecn/p00175/t001/solution.sql
+++ src/main/java/com/potato/study/leetcodecn/p00175/t001/solution.sql
@@ -1,2 +1,34 @@
+-- 175. 组合两个表
+-- SQL架构
+-- 表1: Person
+--
+-- +-------------+---------+
+-- | 列名         | 类型     |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId 是上表主键
+-- 表2: Address
+--
+-- +-------------+---------+
+-- | 列名         | 类型    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId 是上表主键
+--
+--
+-- 编写一个 SQL 查询，满足条件：无论 person 是否有地址信息，都需要基于上述两表提供 person 的以下信息：
+--
+--
+--
+-- FirstName, LastName, City, State
+
+
 # Write your MySQL query statement below
 SELECT FirstName, LastName, City, State FROM  Person as a LEFT JOIN Address as b ON a.PersonId = b.PersonId;
