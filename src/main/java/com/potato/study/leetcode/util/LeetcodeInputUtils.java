1c732aa5d91863318214d7e6ad692e16e1a81aba
liuzhao11 <liuzhao11@meituan.com>
2021-02-13 17:46:14 +0800

    2021.2.13 752. 打开转盘锁 2021.7.18

---
+++ src/main/java/com/potato/study/leetcode/util/LeetcodeInputUtils.java
@@ -0,0 +1,51 @@
+package com.potato.study.leetcode.util;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * leetcode 题目中输入生成的工具类
+ */
+public class LeetcodeInputUtils {
+
+
+    /**
+     * deadends = ["0201","0101","0102","1212","2002"]
+     * 测试用例
+     * []
+     * null
+     * 返回 string 数组
+     * @param input
+     * @return
+     */
+    public static String[] inputString2StringArray(String input) {
+        if (input == null) {
+            return null;
+        }
+        if ("".equals(input) || "[]".equals(input)) {
+            return new String[0];
+        }
+        // 去掉"[]"
+        String substring = input.substring(1, input.length() - 1);
+        // ，拆分
+        String[] split = substring.split(",");
+        // 去掉引号
+        String[] result = new String[split.length];
+        for (int i = 0; i < split.length; i++) {
+            String word = split[i];
+            word = word.substring(1, word.length() - 1);
+            result[i] = word;
+        }
+        // 返回数组
+        return result;
+    }
+
+
+    @Test
+    public void testInputString2StringArray() {
+        String input = "[\"0201\",\"0101\",\"0102\",\"1212\",\"2002\"]";
+        String[] array = inputString2StringArray(input);
+        System.out.println(Arrays.toString(array));
+    }
+}
