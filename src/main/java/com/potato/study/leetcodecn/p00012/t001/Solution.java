46497c40fc81418fdd8cba6272bc9b2357c76f70
liuzhao11 <liuzhao11@meituan.com>
2021-02-14 17:12:38 +0800

    2021.2.14 12. 整数转罗马数字 2021.7.25

---
+++ src/main/java/com/potato/study/leetcodecn/p00012/t001/Solution.java
@@ -0,0 +1,140 @@
+package com.potato.study.leetcodecn.p00012.t001;
+
+import org.junit.Assert;
+
+/**
+ * 12. 整数转罗马数字
+ *
+ * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+
+ 字符          数值
+ I             1
+ V             5
+ X             10
+ L             50
+ C             100
+ D             500
+ M             1000
+ 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+ 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+ I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+
+ 示例 1:
+
+ 输入: 3
+ 输出: "III"
+ 示例 2:
+
+ 输入: 4
+ 输出: "IV"
+ 示例 3:
+
+ 输入: 9
+ 输出: "IX"
+ 示例 4:
+
+ 输入: 58
+ 输出: "LVIII"
+ 解释: L = 50, V = 5, III = 3.
+ 示例 5:
+
+ 输入: 1994
+ 输出: "MCMXCIV"
+ 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+  
+
+ 提示：
+
+ 1 <= num <= 3999
+
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/integer-to-roman
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * 递归实现
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        return intToRoman(num, "");
+    }
+
+
+    /**
+     *
+     * @param num       当前剩下的数组
+     * @param prefix    前缀
+     * @return
+     */
+    private String intToRoman(int num, String prefix) {
+        // 千
+        if (num >= 1000) {
+            return intToRoman(num - 1000, prefix + "M");
+        }
+        // 百
+        if (num >= 900) {
+            return intToRoman(num - 900, prefix + "CM");
+        }
+        if (num >= 500) {
+            return intToRoman(num - 500, prefix + "D");
+        }
+        if (500 > num && num >= 400 ) {
+            return intToRoman(num - 400, prefix + "CD");
+        }
+        if (num >= 100) {
+            return intToRoman(num - 100, prefix + "C");
+        }
+        // 十位
+        if (num >= 90) {
+            return intToRoman(num - 90, prefix + "XC");
+        }
+        if (num >= 50) {
+            return intToRoman(num - 50, prefix + "L");
+        }
+        if (50 > num && num >= 40 ) {
+            return intToRoman(num - 40, prefix + "XL");
+        }
+        if (num >= 10) {
+            return intToRoman(num - 10, prefix + "X");
+        }
+        // 个位
+        if (num >= 9) {
+            return intToRoman(num - 9, prefix + "IX");
+        }
+        if (num >= 5) {
+            return intToRoman(num - 5, prefix + "V");
+        }
+        if (5 > num && num >= 4 ) {
+            return intToRoman(num - 4, prefix + "IV");
+        }
+        if (num >= 1) {
+            return intToRoman(num - 1, prefix + "I");
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String res = solution.intToRoman(3);
+        System.out.println(res);
+        Assert.assertEquals("III", res);
+
+        res = solution.intToRoman(4);
+        System.out.println(res);
+        Assert.assertEquals("IV", res);
+
+        res = solution.intToRoman(1994);
+        System.out.println(res);
+        Assert.assertEquals("MCMXCIV", res);
+    }
+}
