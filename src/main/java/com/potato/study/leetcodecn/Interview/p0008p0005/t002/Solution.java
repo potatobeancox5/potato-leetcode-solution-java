265b3b10bd17c910fcb565fb2d27e1080e1af748
liuzhao11 <liuzhao11@meituan.com>
2021-02-17 14:14:37 +0800

    2021.2.17 面试题 08.05. 递归乘法 2021.8.12

--- src/main/java/com/potato/study/leetcodecn/Interview/p0008p0005/t002/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/Interview/p0008p0005/t002/Solution.java
@@ -1,4 +1,4 @@
-package com.potato.study.leetcodecn.Interview.p0008p0005;
+package com.potato.study.leetcodecn.Interview.p0008p0005.t002;
 
 
 /**
@@ -28,7 +28,7 @@ public class Solution {
 
     /**
      * 倍增 递归实现
-     * 先实现一个迭代版本，
+     * ，
      * 内部思路就是 b变成二进制写法 比如101
      * 说明 有 4个 a + 1个a
      *
@@ -37,17 +37,27 @@ public class Solution {
      * @return
      */
     public int multiply(int a, int b) {
-        int result = 0;
-        while (b > 0) {
-            // 如果当前 末尾是1 那么res 需要加上当前的a
-            if ((b & 1) == 1) {
-                result += a;
-            }
-            // b 右移一个位置，对应的 a需要左移 1个位置，相当于 记录了 010中的1的位置
-            b = b >>> 1;
-            a = a << 1;
+        return multiply(a, b, 0);
+    }
+
+
+    /**
+     * 递归计算倍增结果
+     * @param a
+     * @param b
+     * @param temp
+     * @return
+     */
+    private int multiply(int a, int b, int temp) {
+        if (b == 0) {
+            return temp;
         }
-        return result;
+        if ((b & 1)== 1) {
+            temp += a;
+        }
+        b = b >>> 1;
+        a = a << 1;
+        return multiply(a, b, temp);
     }
 
     public static void main(String[] args) {
