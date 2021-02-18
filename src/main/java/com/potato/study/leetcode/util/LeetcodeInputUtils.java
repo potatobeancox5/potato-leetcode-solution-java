11b643a90186e8678bc0d5f943f540c59db0e879
liuzhao11 <liuzhao11@meituan.com>
2021-02-18 19:14:34 +0800

    2021.2.18 1725. 可以形成最大正方形的矩形数目 2021.9.1

--- src/main/java/com/potato/study/leetcode/util/LeetcodeInputUtils.java
+++ src/main/java/com/potato/study/leetcode/util/LeetcodeInputUtils.java
@@ -2,7 +2,9 @@ package com.potato.study.leetcode.util;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * leetcode 题目中输入生成的工具类
@@ -47,5 +49,64 @@ public class LeetcodeInputUtils {
         String input = "[\"0201\",\"0101\",\"0102\",\"1212\",\"2002\"]";
         String[] array = inputString2StringArray(input);
         System.out.println(Arrays.toString(array));
+    }
+
+
+    /**
+     * rectangles = [[2,3],[3,7],[4,3],[3,7]]
+     * 测试用例
+     * []
+     * null
+     * 返回 int 2维 数组
+     * @param input
+     * @return
+     */
+    public static int[][] inputString2IntArrayTwoDimensional(String input) {
+        if (input == null) {
+            return null;
+        }
+        if ("".equals(input) || "[]".equals(input)) {
+            return new int[0][0];
+        }
+        // 去掉"[2,3],[3,7],[4,3],[3,7] 或者 [1,2]"
+        String substring = input.substring(1, input.length() - 1);
+        // ，拆分
+        String[] split = substring.split("]");
+//        String[] result = new String[split.length];
+        int[][] result = new int[split.length][];
+        for (int i = 0; i < split.length; i++) {
+            // 去掉引号 [2,3 或者 ,[3,7
+            String word = split[i].trim();
+            if ("".equals(word)) {
+                continue;
+            }
+            // [2 或者 3 或者 ""
+            String[] numSplit = word.split(",");
+            List<Integer> list = new ArrayList<>();
+            for (String numStr : numSplit) {
+                if (numStr.startsWith("[")) {
+                    numStr = numStr.substring(1);
+                }
+                if ("".equals(numStr)) {
+                    continue;
+                }
+                list.add(Integer.parseInt(numStr));
+            }
+            int[] arr = new int[list.size()];
+            for (int j = 0; j < list.size(); j++) {
+                arr[j] = list.get(j);
+            }
+            result[i] = arr;
+        }
+        // 返回数组
+        return result;
+    }
+
+
+    @Test
+    public void testInputString2IntArrayTwoDimensional() {
+        String input = "[[2,3],[3,7],[4,3],[3,7]]";
+        int[][] array = inputString2IntArrayTwoDimensional(input);
+        System.out.println(Arrays.deepToString(array));
     }
 }
