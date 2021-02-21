6ff6f0b5543bbd347d667cb3078e6d2258301b62
liuzhao11 <liuzhao11@meituan.com>
2021-02-21 13:21:13 +0800

    2021.2.21 剑指 Offer 64. 求1+2+…+n 2021.9.15

---
+++ src/main/java/com/potato/study/leetcodecn/sword2offer/p0064/p1/t001/Solution.java
@@ -0,0 +1,45 @@
+package com.potato.study.leetcodecn.sword2offer.p0064.p1.t001;
+
+
+/**
+ * 剑指 Offer 64. 求1+2+…+n
+ *
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+  
+
+ 示例 1：
+
+ 输入: n = 3
+ 输出: 6
+ 示例 2：
+
+ 输入: n = 9
+ 输出: 45
+  
+
+ 限制：
+
+ 1 <= n <= 10000
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public int sumNums(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sumNums(n-1);
+    }
+
+
+}
