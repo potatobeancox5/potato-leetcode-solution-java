364d5e1b745d6238f4c633e09000a083ab6ae04e
liuzhao11 <liuzhao11@meituan.com>
2021-02-21 21:34:31 +0800

    2021.2.21 43. 字符串相乘 2021.9.20

--- src/main/java/com/potato/study/leetcodecn/p00043/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00043/t001/Solution.java
@@ -30,63 +30,130 @@ import org.junit.Assert;
 public class Solution {
 
     /**
-     * 我这个算的 太sb了
+     * 先个位乘法再算加法
      * @param num1
      * @param num2
      * @return
      */
     public String multiply(String num1, String num2) {
-        if (null == num1 || num2 == null) {
-            return null;
+        if ("0".equals(num2) || "0".equals(num1)) {
+            return "0";
         }
-        if (num1.length() == 0 || num2.length() == 0) {
-            return null;
+        // 遍历 num2 的每个 位置 求基础
+        int index = num2.length() - 1;
+        int zeroCount = 0;
+        String result = "0";
+        while (index >= 0) {
+            char num2Char = num2.charAt(index);
+            String target = this.multiplyBy(num1, num2Char, zeroCount);
+            result = plus(result, target);
+            index--;
+            zeroCount++;
         }
+        return result;
+    }
+
+
+    /**
+     * num1 * num2 进位并在末尾补上0
+     * @param num1Str
+     * @param ch2    ch2 只能有一个数字
+     * @param zeroCount
+     * @return
+     */
+    private String multiplyBy(String num1Str, char ch2, int zeroCount) {
+        if ('0' == ch2){
+            return "0";
+        }
+        int index = num1Str.length() - 1;
+        long process = 0;
+        long num2 = (ch2 - '0');
+        StringBuilder builder = new StringBuilder();
+        while (index >= 0) {
+            char cc = num1Str.charAt(index);
+            long num1 = cc - '0';
+            long res = num1 * num2 + process;
+            if (res >= 10) {
+                process = res / 10;
+                res %= 10;
+            } else {
+                process = 0;
+            }
+            builder.insert(0, res);
+            index--;
+        }
+        if (process > 0) {
+            builder.insert(0, process);
+        }
+        // 填充 0
+        for (int i = 0; i < zeroCount; i++) {
+            builder.append(0);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 求和
+     * @param num1
+     * @param num2
+     * @return
+     */
+    private String plus(String num1, String num2) {
+        boolean process = false;
+        StringBuilder builder = new StringBuilder();
         int index1 = num1.length() - 1;
         int index2 = num2.length() - 1;
-        int process = 0;
-        StringBuilder builder = new StringBuilder();
         while (index1 >= 0 && index2 >= 0) {
-            int n1 = num1.charAt(index1) - '0';
-            int n2 = num2.charAt(index2) - '0';
-
-            int tmp = n1 * n2 + process;
-            if (tmp > 9) {
-                process = tmp / 10;
-                tmp %= 10;
+            long n1 = num1.charAt(index1) - '0';
+            long n2 = num2.charAt(index2) - '0';
+            long tmp = n1 + n2;
+            if (process) {
+                tmp++;
             }
-            builder.append(tmp);
+            if (tmp > 9) {
+                tmp %= 10;
+                process = true;
+            } else {
+                process = false;
+            }
+            builder.insert(0, tmp);
             index1--;
             index2--;
         }
         while (index1 >= 0) {
-            int n1 = num1.charAt(index1) - '0';
-
-            int tmp = n1 + process;
-            if (tmp > 9) {
-                process = tmp / 10;
-                tmp %= 10;
+            long n1 = num1.charAt(index1) - '0';
+            long tmp = n1;
+            if (process) {
+                tmp++;
             }
-            builder.append(tmp);
+            if (tmp > 9) {
+                tmp %= 10;
+                process = true;
+            } else {
+                process = false;
+            }
+            builder.insert(0, tmp);
             index1--;
         }
         while (index2 >= 0) {
-            int n2 = num2.charAt(index2) - '0';
-
-            int tmp = n2 + process;
-            if (tmp > 9) {
-                process = tmp / 10;
-                tmp %= 10;
+            long n2 = num2.charAt(index2) - '0';
+            long tmp = n2;
+            if (process) {
+                tmp++;
             }
-            builder.append(tmp);
+            if (tmp > 9) {
+                tmp %= 10;
+                process = true;
+            } else {
+                process = false;
+            }
+            builder.insert(0, tmp);
             index2--;
         }
-
-        if (process > 0) {
-            builder.append(process);
+        if (process) {
+            builder.insert(0, 1);
         }
-
-        return builder.reverse().toString();
+        return builder.toString();
     }
 
     public static void main(String[] args) {
@@ -103,5 +170,20 @@ public class Solution {
         multiply = solution.multiply(num1, num2);
         System.out.println(multiply);
         Assert.assertEquals("56088", multiply);
+
+//        num1 = "123456789";
+//        num2 = "987654321";
+//        multiply = solution.multiply(num1, num2);
+//        System.out.println(multiply);
+//        Assert.assertEquals("121932631112635269", multiply);
+
+
+        num1 = "123456789";
+        num2 = "3";
+        multiply = solution.multiply(num1, num2);
+        System.out.println(multiply);
+        Assert.assertEquals("370370367", multiply);
+
+
     }
 }
