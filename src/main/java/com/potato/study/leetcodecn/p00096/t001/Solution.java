5e33e862e9550e8467e82c54d16e9570cfb40231
liuzhao11 <liuzhao11@meituan.com>
2021-02-28 09:17:04 +0800

    2021.2.28 96. 不同的二叉搜索树 2021.10.07

--- src/main/java/com/potato/study/leetcodecn/p00096/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00096/t001/Solution.java
@@ -1,90 +1,96 @@
 package com.potato.study.leetcodecn.p00096.t001;
 
 
-import com.potato.study.leetcode.domain.TreeNode;
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 98. 验证二叉搜索树
+ * 96. 不同的二叉搜索树
  *
- * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
 
- 假设一个二叉搜索树具有如下特征：
+ 示例:
 
- 节点的左子树只包含小于当前节点的数。
- 节点的右子树只包含大于当前节点的数。
- 所有左子树和右子树自身必须也是二叉搜索树。
- 示例 1:
+ 输入: 3
+ 输出: 5
+ 解释:
+ 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
 
- 输入:
- 2
- / \
- 1   3
- 输出: true
- 示例 2:
-
- 输入:
- 5
- / \
- 1   4
-      / \
-     3   6
- 输出: false
- 解释: 输入为: [5,1,4,null,null,3,6]。
-      根节点的值为 5 ，但是其右子节点值为 4 。
+ 1         3     3      2      1
+ \       /     /      / \      \
+ 3     2     1      1   3      2
+ /     /       \                 \
+ 2     1         2                 3
 
  来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  */
 public class Solution {
 
-    /**
-     * 按照定义 当前节点 大于左孩子最大值，小于右子树的最小值
-     * @param root
-     * @return
-     */
-    public boolean isValidBST(TreeNode root) {
-        if (null == root) {
-            return true;
-        }
-        if (root.left == null && root.right == null){
-            return true;
-        }
-        return isAllChildSmall(root.val, root.left) && isAllChildBig(root.val, root.right)
-                && isValidBST(root.left) && isValidBST(root.right);
-    }
-
-    private boolean isAllChildBig(int val, TreeNode node) {
-        if (null == node) {
-            return true;
-        }
-        if (node.val <= val) {
-            return false;
-        }
-        // 孩子呢
-        return isAllChildBig(val, node.left) && isAllChildBig(val, node.right);
-    }
+    private Map<String, Integer> countMap;
 
     /**
-     * node 和 他的孩子是否不超过val
-     * @param val
-     * @param node
+     * 也是 dfs 任意 从 1-n 选择一个点作为 当前的节点值 然后将 【1， n】 划分为 【1， i-1】，【i+1，n】递归生成子树 ，统计数字即可
+     * @param n
      * @return
      */
-    private boolean isAllChildSmall(int val, TreeNode node) {
-        if (null == node) {
-            return true;
-        }
-        if (node.val >= val) {
-            return false;
-        }
-        // 孩子呢
-        return isAllChildSmall(val, node.left) && isAllChildSmall(val, node.right);
+    public int numTrees(int n) {
+        countMap = new HashMap<>();
+        return getNumTrees(1, n);
     }
 
+
+    /**
+     * 递归获取 数量
+     * @param start
+     * @param end
+     * @return
+     */
+    private int getNumTrees(int start, int end) {
+        // 0 map 记忆搜索剪枝
+        String key = start + "_" + end;
+        Integer count = countMap.get(key);
+        if (count != null) {
+            return count;
+        }
+        // 终止条件 递归
+        if (start == end) {
+            return 1;
+        }
+        if (start > end) {
+            return 0;
+        }
+        // 1. 任意 从 1-n 选择一个点作为 当前的节点值 然后将
+        int totalCount = 0;
+        for (int i = start; i <= end; i++) {
+            // 2. 然后将 【1， n】 划分为 【1， i-1】，【i+1，n】递归生成子树 ，统计数字即可 用一个 map 缓存 结果
+            int left = getNumTrees(start, i - 1);
+            int right = getNumTrees(i + 1, end);
+            // left 和 right 组合数
+            if (left == 0 || right == 0) {
+                totalCount += (left + right);
+            } else {
+                totalCount += left * right;
+            }
+
+        }
+        // 3. map 缓存 结果
+        countMap.put(key, totalCount);
+        return totalCount;
+    }
 
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        int numTrees = solution.numTrees(1);
+        System.out.println(numTrees);
+        Assert.assertSame(1, numTrees);
 
+        numTrees = solution.numTrees(3);
+        System.out.println(numTrees);
+        Assert.assertSame(5, numTrees);
     }
 }
