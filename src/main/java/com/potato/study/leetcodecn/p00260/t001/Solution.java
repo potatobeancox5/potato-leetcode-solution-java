88ff8c28cb166f45e4e6dbe9c0c136a9cf0e8aa2
liuzhao11 <liuzhao11@meituan.com>
2021-03-06 15:14:25 +0800

    2021.3.6 260. 只出现一次的数字 III 2021.10.24

--- src/main/java/com/potato/study/leetcodecn/p00260/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00260/t001/Solution.java
@@ -1,5 +1,7 @@
 package com.potato.study.leetcodecn.p00260.t001;
 
+import java.util.Arrays;
+
 /**
  * 260. 只出现一次的数字 III
  *
@@ -22,14 +24,39 @@ package com.potato.study.leetcodecn.p00260.t001;
 public class Solution {
 
     /**
-     * 一直异或
+     * 一直异或 能得到  n1 ^ n2 结果
+     * 找到一个位置 一个为1的位置 按照这个位置，将nums 分成两步分
+     * 对于 部分1 进行 遍历 异或
+     * 对于 部分2 进行 遍历 异或
      * @param nums
      * @return
      */
     public int[] singleNumber(int[] nums) {
-        if (null == nums) {
-
+        int n = 0;
+        for (int num : nums) {
+            n ^= num;
         }
-        return nums;
+        // 找到区分位置
+        int bit = 1;
+        while ((bit & n) == 0) {
+            bit <<= 1;
+        }
+        int n1 = 0;
+        int n2 = 0;
+        for (int num : nums) {
+            if ((bit & num) > 0) {
+                n1 ^= num;
+            } else {
+                n2 ^= num;
+            }
+        }
+        return new int[]{n1, n2};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[] {1,2,1,3,2,5};
+        int[] res = solution.singleNumber(nums);
+        System.out.println(Arrays.toString(res));
     }
 }
