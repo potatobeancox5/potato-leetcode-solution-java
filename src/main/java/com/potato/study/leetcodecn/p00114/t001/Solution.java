37be07910a14e428a11b5b1b2079182b313f3334
liuzhao11 <liuzhao11@meituan.com>
2021-03-19 19:29:16 +0800

    2021.3.15 114. 二叉树展开为链表 2021.12.2

--- src/main/java/com/potato/study/leetcodecn/p00114/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00114/t001/Solution.java
@@ -38,12 +38,54 @@ import com.potato.study.leetcode.domain.TreeNode;
  */
 public class Solution {
 
+
+    private TreeNode pre;
+
     /**
-     * 先序 遍历
+     * 先序 遍历 展开的时候 改变  right 指向
      * @param root
      */
     public void flatten(TreeNode root) {
+        // visit this node
+        if (root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        if (pre != null) {
+            pre.right = root;
+        }
+        pre = root;
+        pre.left = null;
+        // visit left
+        if (left != null) {
+            flatten(left);
+        }
+        // visit right
+        if (right != null) {
+            flatten(right);
+        }
+    }
 
+
+    /**
+     *
+     * @param root      当前需要修改的节点
+     * @param isLeft    当前节点是不是左左孩子
+     * @param last      上一个遍历的节点
+     */
+    private void preorderTraverse(TreeNode root, boolean isLeft, TreeNode last) {
+        // 终止条件
+//        if (null == root) {
+//            return;
+//        }
+//        // 没有左孩子
+//        if (root.left == null) {
+//            last.right = ;
+//        }
+//        preorderTraverse(root.left);
+//        // it
+//        root.right = preorderTraverse(root.right);
     }
 
     public static void main(String[] args) {
