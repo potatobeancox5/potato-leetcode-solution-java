daf0dddcba7efe6e425886389dc7828b434938e7
liuzhao03 <Liu55555%>
2021-05-14 21:23:08 +0800

    2021.5.14 872. 叶子相似的树 2022.3.20

---
+++ src/main/java/com/potato/study/leetcodecn/p00872/t001/Solution.java
@@ -0,0 +1,101 @@
+package com.potato.study.leetcodecn.p00872.t001;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+/**
+ * 872. 叶子相似的树
+ *
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ *
+ *
+ *
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ *
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ *
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：root1 = [1], root2 = [1]
+ * 输出：true
+ * 示例 3：
+ *
+ * 输入：root1 = [1], root2 = [2]
+ * 输出：false
+ * 示例 4：
+ *
+ * 输入：root1 = [1,2], root2 = [2,2]
+ * 输出：true
+ * 示例 5：
+ *
+ *
+ *
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 给定的两棵树可能会有 1 到 200 个结点。
+ * 给定的两棵树上的值介于 0 到 200 之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/leaf-similar-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     *
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leafList1 = getLeafList(root1);
+        List<Integer> leafList2 = getLeafList(root2);
+        return leafList1.equals(leafList2);
+    }
+
+
+    /**
+     * 使用栈模拟 生成树叶的遍历结果
+     * @param root
+     * @return
+     */
+    private List<Integer> getLeafList(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+            if (pop.left == null && pop.right == null) {
+                result.add(pop.val);
+            }
+            if (pop.right != null) {
+                stack.add(pop.right);
+            }
+            if (pop.left != null) {
+                stack.add(pop.left);
+            }
+        }
+        return result;
+    }
+}
