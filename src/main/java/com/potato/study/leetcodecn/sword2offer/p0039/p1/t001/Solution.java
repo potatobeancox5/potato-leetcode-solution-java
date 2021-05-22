7d1c1e611c3db81790ca60de3d9c426a2bcda66f
liuzhao11 <liuzhao11@sankuai.com>
2021-05-22 22:57:10 +0800

    2021.5.22 剑指 Offer 39. 数组中出现次数超过一半的数字 2022.4.6

---
+++ src/main/java/com/potato/study/leetcodecn/sword2offer/p0039/p1/t001/Solution.java
@@ -0,0 +1,61 @@
+package com.potato.study.leetcodecn.sword2offer.p0039.p1.t001;
+
+import java.util.Arrays;
+
+/**
+ * 剑指 Offer 39. 数组中出现次数超过一半的数字
+ *
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+  
+
+ 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+  
+
+ 示例 1:
+
+ 输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+ 输出: 2
+  
+
+ 限制：
+
+ 1 <= 数组长度 <= 50000
+
+  
+
+ 注意：本题与主站 169 题相同：https://leetcode-cn.com/problems/majority-element/
+
+
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+    /**
+     * 超过数组的一半
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int vote = 0;
+        int target = 0;
+        for (int num : nums) {
+            if (vote == 0) {
+                target = num;
+                vote++;
+                continue;
+            }
+            if (target == num) {
+                vote++;
+            } else {
+                vote--;
+            }
+        }
+        return target;
+    }
+
+}
