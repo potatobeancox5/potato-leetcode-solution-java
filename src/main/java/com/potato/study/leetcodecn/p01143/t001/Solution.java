c75ead5f16823463672400ceb03715f8ac03bb53
liuzhao11 <liuzhao11@sankuai.com>
2021-05-23 22:59:43 +0800

    2021.5.22 1143. 最长公共子序列 2022.4.8

--- src/main/java/com/potato/study/leetcodecn/p01143/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01143/t001/Solution.java
@@ -60,23 +60,22 @@ public class Solution {
      */
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        dp[0][0] = 0;
+        int max = 0;
         for (int j = 1; j < text2.length() + 1; j++) {
             for (int i = 1; i < text1.length() + 1; i++) {
-                // 往里找找到最大的
-                int max = 0;
-                for (int k = 0; k < i; k++) {
-                    for (int l = 0; l < j; l++) {
-                        max = Math.max(max, dp[k][l]);
-                    }
+                // 按照地推公式计算
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] =  1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j]);
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]);
                 }
-                if (text1.charAt(i-1) != text2.charAt(j-1)) {
-                    dp[i][j] = Math.max(dp[i][j], max);
-                    continue;
-                }
-                dp[i][j] = Math.max(dp[i][j], 1 + max);
+                max = Math.max(dp[i][j], max);
             }
         }
-        return dp[text1.length()][text2.length()];
+        return max;
     }
 
     public static void main(String[] args) {
