e663d033130ded18b8df4cd96acfc595e935b79e
liuzhao11 <liuzhao11@sankuai.com>
2021-05-29 23:20:45 +0800

    2021.5.29 1013. 将数组分成和相等的三个部分 2022.4.16

--- src/main/java/com/potato/study/leetcodecn/p00227/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00227/t001/Solution.java
@@ -1,5 +1,9 @@
 package com.potato.study.leetcodecn.p00227.t001;
 
+import org.junit.Assert;
+
+import java.util.Stack;
+
 /**
  * 227. 基本计算器 II
  *
@@ -40,11 +44,104 @@ public class Solution {
 
     /**
      * 操作数栈 和 运算符 栈
+     *
      * @param s
      * @return
      */
     public int calculate(String s) {
+        if (null == s || s.length() == 0) {
+            return 0;
+        }
+        // 干掉空格
+        s = s.replace(" ", "");
+        Stack<Integer> numStack = new Stack<>();
+        Stack<Character> opStack = new Stack<>();
+        // 遍历 s 如果当前是数字 那就 一直往后找数字 数字入栈
+        int index = 0;
+        while (index < s.length()) {
+            char ch = s.charAt(index);
+            if (Character.isDigit(ch)) {
+                // 如果目前是 运算符，运算符根 运算符栈比较 如果优先级低，那么 pop 两个数字运算入栈，否则 运算符入栈
+                int num = 0;
+                while (index < s.length() && Character.isDigit(s.charAt(index))) {
+                    num *= 10;
+                    num += (s.charAt(index) - '0');
+                    index++;
+                }
+                numStack.add(num);
+            } else {
+                if (!opStack.isEmpty() && compareOp(opStack.peek(), ch) <= 0) {
+                    while (!opStack.isEmpty() && compareOp(opStack.peek(), ch) <= 0) {
+                        Character op = opStack.pop();
+                        int num1 = numStack.pop();
+                        int num2 = numStack.pop();
+                        int target = compute(num2, num1, op);
+                        numStack.add(target);
+                    }
+                }
+                opStack.add(ch);
+                index++;
+            }
+        }
+        // 最后 一直出栈 计算
+        while (!opStack.isEmpty()) {
+            Character op = opStack.pop();
+            int num1 = numStack.pop();
+            int num2 = numStack.pop();
+            int target = compute(num2, num1, op);
+            numStack.add(target);
+        }
+        return numStack.peek();
+    }
+
+    /**
+     * 计算
+     * @param num1
+     * @param num2
+     * @param op
+     * @return
+     */
+    private int compute(int num1, int num2, char op) {
+        int num;
+        if (op == '+') {
+            num = num1 + num2;
+        } else if (op == '-') {
+            num = num1 - num2;
+        } else if (op == '*') {
+            num = num1 * num2;
+        } else {
+            num = num1 / num2;
+        }
+        return num;
+    }
+
+    /**
+     * 判断两个运算符的优先级
+     * @param op1
+     * @param op2
+     * @return -1 优先运算 op1， 1 优先运算 op2
+     */
+    private int compareOp(char op1, char op2) {
+        if ('*' == op1 || '/' == op1) {
+            return -1;
+        }
+        if ('*' == op2 || '/' == op2) {
+            return 1;
+        }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "3+2*2";
+        int num = solution.calculate(s);
+        System.out.println(num);
+        Assert.assertEquals(7, num);
+
+        s = "3/2";
+        num = solution.calculate(s);
+        System.out.println(num);
+        Assert.assertEquals(1, num);
     }
 
 }
