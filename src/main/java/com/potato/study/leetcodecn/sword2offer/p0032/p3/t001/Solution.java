88bcc8754c77304efc87ab8d904d4d9882f7cf66
liuzhao03 <Liu55555%>
2021-05-30 20:18:53 +0800

    2021.5.26 剑指 Offer 32 - III. 从上到下打印二叉树 III 2022.4.15

---
+++ src/main/java/com/potato/study/leetcodecn/sword2offer/p0032/p3/t001/Solution.java
@@ -0,0 +1,81 @@
+package com.potato.study.leetcodecn.sword2offer.p0032.p3.t001;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+/**
+ * 剑指 Offer 32 - III. 从上到下打印二叉树 III
+ *
+ * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+ *
+ *  
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ *  
+ *
+ * 提示：
+ *
+ * 节点总数 <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> list = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (index % 2 == 0) {
+                    list.add(poll.val);
+                } else {
+                    list.add(0, poll.val);
+                }
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            result.add(list);
+            index++;
+        }
+        return result;
+    }
+
+}
