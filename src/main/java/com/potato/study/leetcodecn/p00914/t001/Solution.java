5d805eaf90fa441aca7f56edb6f04fc3b8c69b06
liuzhao03 <Liu55555%>
2021-06-03 12:40:20 +0800

    2021.6.2 914. 卡牌分组 2022.4.28

--- src/main/java/com/potato/study/leetcodecn/p00914/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00914/t001/Solution.java
@@ -1,9 +1,15 @@
 package com.potato.study.leetcodecn.p00914.t001;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+
+import org.junit.Assert;
 
 /**
  * 914. 卡牌分组
@@ -56,41 +62,66 @@ import java.util.Map;
  */
 public class Solution {
 
+    /**
+     * map 计数 找到最小的那个count —— min 看一下 其他的count 是不是都是 他的倍数
+     * @param deck
+     * @return
+     */
     public boolean hasGroupsSizeX(int[] deck) {
         if (null == deck || deck.length == 0) {
             return true;
         }
         Map<Integer, Integer> countMap = new HashMap<>();
+        int maxCount = Integer.MIN_VALUE;
         for (int num : deck) {
             Integer count = countMap.getOrDefault(num, 0);
             count++;
             countMap.put(num, count);
+            maxCount = Math.max(maxCount, count);
         }
-        // 遍历 比较能够构成
         List<Integer> countList = new ArrayList<>(countMap.values());
-        int target = countList.get(0);
-        if (target == 1) {
+        if (0 == countList.size()) {
             return false;
         }
-        for (int i = 1; i < countList.size(); i++) {
-            if (countList.get(i) != target) {
-                return false;
+        // 找到最小的 count 求他的因数 然后判定 每个因数 是不是其他count 的因数 （找到整个数组的公因数）
+        boolean[] isPrime = new boolean[maxCount+1];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i <= maxCount; i++) {
+            if (isPrime[i]) {
+                // 遍历 countList 看看 是不是都能 整除
+                boolean isValid = true;
+                for (int val : countList) {
+                    if (val % i != 0) {
+                        isValid = false;
+                    }
+                }
+                // 已经有了可以整除的
+                if (isValid) {
+                    return isValid;
+                }
+                // i * i 开始设置非素数
+                for (int j = i; j * i <= maxCount; j++) {
+                    isPrime[j * i] = false;
+                }
+            } else {
+                continue;
             }
         }
-        return true;
+        return false;
     }
 
-//    public static void main(String[] args) {
-//        Solution solution = new Solution();
-//        int[] arr = new int[] {4,2,5,7};
-//        int[] ints = solution.sortArrayByParityII(arr);
-//        System.out.println(Arrays.toString(ints)); // [4,5,2,7]
-//
-//
-//        arr = new int[] {2,0,3,4,1,3};
-//        ints = solution.sortArrayByParityII(arr);
-//        System.out.println(Arrays.toString(ints)); // [2,3,0,1,4,3]
-//    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = new int[] {1,2,3,4,4,3,2,1};
+        boolean res = solution.hasGroupsSizeX(arr);
+        System.out.println(res); // [4,5,2,7]
+        Assert.assertEquals(true, res);
+
+        arr = new int[] {1,1,1,2,2,2,3,3};
+        res = solution.hasGroupsSizeX(arr);
+        System.out.println(res); // [4,5,2,7]
+        Assert.assertEquals(false, res);
+    }
 
 
 }
