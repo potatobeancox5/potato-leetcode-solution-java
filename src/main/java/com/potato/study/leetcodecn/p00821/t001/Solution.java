4e8898b10af4faec2a9690b635dd365b3f831ab5
liuzhao11 <liuzhao11@sankuai.com>
2021-06-06 22:35:09 +0800

    2021.6.6 821. 字符的最短距离 2022.5.19

--- src/main/java/com/potato/study/leetcodecn/p00821/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00821/t001/Solution.java
@@ -1,6 +1,11 @@
 package com.potato.study.leetcodecn.p00821.t001;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * 821. 字符的最短距离
@@ -30,6 +35,10 @@ public class Solution {
 
     /**
      *
+     * https://blog.csdn.net/Mercuriooo/article/details/100406505
+     * SortedSet 使用方法
+     * 1. 遍历 s 找到 每次 c出现的下标 使用 SortedSet 保存下标
+     * 2. 遍历 s 找到 第一个大于index 下表的e 的first SortedSet tailSet(fromElement):子集,由大于fromElement的元素组成
      * @param s
      * @param c
      * @return
@@ -38,10 +47,28 @@ public class Solution {
         if (null == s || "".equals(s)) {
             return new int[]{};
         }
-        int[] res = new int[s.length()];
+        // 默认升序
+        SortedSet<Integer> sortedSet = new TreeSet<>();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            res[i] = Math.abs((int)ch - (int)c);
+            if (s.charAt(i) == c) {
+                sortedSet.add(i);
+            }
+        }
+        int[] res = new int[s.length()];
+        Arrays.fill(res, Integer.MAX_VALUE);
+        for (int i = 0; i < res.length; i++) {
+            SortedSet<Integer> tailSet = sortedSet.tailSet(i);
+            if (!tailSet.isEmpty()) {
+                Integer target1 = tailSet.first();
+                // 求距离
+                res[i] = Math.abs(target1 - i);
+            }
+
+            SortedSet<Integer> headSet = sortedSet.headSet(i);
+            if (!headSet.isEmpty()) {
+                Integer target2 = headSet.last();
+                res[i] = Math.min(res[i], Math.abs(target2 - i));
+            }
         }
         return res;
     }
@@ -51,6 +78,8 @@ public class Solution {
         String s = "loveleetcode";
         char c = 'e';
         int[] ints = solution.shortestToChar(s, c);
+        // [3,2,1,0,1,0,0,1,2,2,1,0]
+        // [3,2,1,0,1,0,0,1,2,2,1,0]
         System.out.println(Arrays.toString(ints));
     }
 }
