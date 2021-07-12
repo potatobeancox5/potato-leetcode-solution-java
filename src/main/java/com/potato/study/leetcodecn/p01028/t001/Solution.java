e9b4df5a6235f10c9f859a67d26708cd2d12adb0
liuzhao03 <>
2021-07-12 16:29:59 +0800

    2021.7.12 1028. 从先序遍历还原二叉树 2022.8.10

--- src/main/java/com/potato/study/leetcodecn/p01028/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01028/t001/Solution.java
@@ -2,8 +2,11 @@ package com.potato.study.leetcodecn.p01028.t001;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import com.potato.study.leetcode.domain.TreeNode;
+
+import sun.jvm.hotspot.debugger.Page;
 
 /**
  * 1028. 从先序遍历还原二叉树
@@ -53,94 +56,146 @@ public class Solution {
 
 
     /**
-     * 递归生成
+     * 用一个栈模拟 先序遍历 过程
+     * stack 中记录 目前还没有完全用到的节点
      * @param traversal
      * @return
      */
     public TreeNode recoverFromPreorder(String traversal) {
-        if (null == traversal || "".equals(traversal)) {
-            return null;
-        }
-        // 遍历 traversal 生成 一个list LevelNode
-        List<LevelNode> list = new ArrayList<>();
         int index = 0;
+        Stack<TreeNode> stack = new Stack<>();
         while (index < traversal.length()) {
-            char ch = traversal.charAt(index);
-            int num = 0;
+            // 遍历 traversal 每一个位置 如果是  --- 循环记录层数
             int level = 0;
-            if (Character.isDigit(ch)) {
-                while (index < traversal.length()
-                        && Character.isDigit(traversal.charAt(index))) {
-                    num *= 10;
-                    num += (ch - '0');
-                    index++;
+            while (index < traversal.length() &&
+                    traversal.charAt(index) == '-') {
+                level++;
+                index++;
+            }
+            int num = 0;
+            // 如果是数字 循环生成数字
+            while (index < traversal.length() &&
+                    Character.isDigit(traversal.charAt(index))) {
+                num *= 10;
+                num += (traversal.charAt(index) - '0');
+                index++;
+            }
+            // 生成几点
+            TreeNode node = new TreeNode(num);
+            // 对于上面生成的记录 如果当前 层数 level 等于 stack 的个数，说明 这个点是peek的左孩子 左孩子生成完 push
+            if (stack.size() == level) {
+                if (!stack.isEmpty()) {
+                    stack.peek().left = node;
                 }
+                stack.push(node);
             } else {
-                // --
-                while (index < traversal.length()
-                        && traversal.charAt(index) == '-') {
-                    index++;
-                    level++;
+                // 否则 循环pop 找到 他的层级一样的节点，此时 节点就是 有孩子
+                while (stack.size() > level) {
+                    stack.pop();
                 }
-            }
-            LevelNode levelNode = new LevelNode();
-            levelNode.val = num;
-            levelNode.level = level;
-            list.add(levelNode);
-        }
-        // 递归求解
-        return generateTree(list);
-    }
-
-    /**
-     * 使用 list 生成树
-     * @param list
-     * @return
-     */
-    private TreeNode generateTree(List<LevelNode> list) {
-        // 终止条件 list 为空 或者 list 只有一个节点
-        if (null == list || list.size() == 0) {
-            return null;
-        }
-        // 使用 list get 0 生成 这个树的头部
-        LevelNode levelNode = list.get(0);
-        TreeNode root = new TreeNode(levelNode.val);
-        int level = levelNode.level;
-        Integer leftStart = null;
-        Integer rightStart = null;
-        for (int i = 1; i < list.size(); i++) {
-            LevelNode node = list.get(i);
-            if (node.level == level + 1) {
-                if (leftStart == null) {
-                    leftStart = i;
-                } else {
-                    rightStart = i;
+                if (!stack.isEmpty()) {
+                    stack.peek().right = node;
                 }
+                stack.push(node);
             }
         }
-        // 在list 中 找到 root level + 1的两个点，利用 这两个点 生成 左右孩子
-        if (leftStart != null) {
-            List<LevelNode> leftList = list.subList(leftStart, rightStart);
-            root.left = generateTree(leftList);
+        // 最后 stack 的栈底就是 返回的根
+        while (stack.size() > 1) {
+            stack.pop();
         }
-        if (rightStart != null) {
-            List<LevelNode> rightList = list.subList(rightStart, list.size());
-            root.right = generateTree(rightList);
-        }
-        return root;
+        return stack.peek();
     }
 
-    class LevelNode {
-        // 值
-        public int val;
-        // 层级
-        public int level;
-    }
+//    /**
+//     * 递归生成
+//     * @param traversal
+//     * @return
+//     */
+//    public TreeNode recoverFromPreorder(String traversal) {
+//        if (null == traversal || "".equals(traversal)) {
+//            return null;
+//        }
+//        // 遍历 traversal 生成 一个list LevelNode
+//        List<LevelNode> list = new ArrayList<>();
+//        int index = 0;
+//        while (index < traversal.length()) {
+//            char ch = traversal.charAt(index);
+//            int num = 0;
+//            int level = 0;
+//            if (Character.isDigit(ch)) {
+//                while (index < traversal.length()
+//                        && Character.isDigit(traversal.charAt(index))) {
+//                    num *= 10;
+//                    num += (ch - '0');
+//                    index++;
+//                }
+//            } else {
+//                // --
+//                while (index < traversal.length()
+//                        && traversal.charAt(index) == '-') {
+//                    index++;
+//                    level++;
+//                }
+//            }
+//            LevelNode levelNode = new LevelNode();
+//            levelNode.val = num;
+//            levelNode.level = level;
+//            list.add(levelNode);
+//        }
+//        // 递归求解
+//        return generateTree(list);
+//    }
+//
+//    /**
+//     * 使用 list 生成树
+//     * @param list
+//     * @return
+//     */
+//    private TreeNode generateTree(List<LevelNode> list) {
+//        // 终止条件 list 为空 或者 list 只有一个节点
+//        if (null == list || list.size() == 0) {
+//            return null;
+//        }
+//        // 使用 list get 0 生成 这个树的头部
+//        LevelNode levelNode = list.get(0);
+//        TreeNode root = new TreeNode(levelNode.val);
+//        int level = levelNode.level;
+//        Integer leftStart = null;
+//        Integer rightStart = null;
+//        for (int i = 1; i < list.size(); i++) {
+//            LevelNode node = list.get(i);
+//            if (node.level == level + 1) {
+//                if (leftStart == null) {
+//                    leftStart = i;
+//                } else {
+//                    rightStart = i;
+//                }
+//            }
+//        }
+//        // 在list 中 找到 root level + 1的两个点，利用 这两个点 生成 左右孩子
+//        if (leftStart != null) {
+//            List<LevelNode> leftList = list.subList(leftStart, rightStart);
+//            root.left = generateTree(leftList);
+//        }
+//        if (rightStart != null) {
+//            List<LevelNode> rightList = list.subList(rightStart, list.size());
+//            root.right = generateTree(rightList);
+//        }
+//        return root;
+//    }
+//
+//    class LevelNode {
+//        // 值
+//        public int val;
+//        // 层级
+//        public int level;
+//    }
 
 
     public static void main(String[] args) {
-//        Solution solution = new Solution();
-//        TreeNode treeNode = solution.recoverFromPreorder();
-
+        Solution solution = new Solution();
+        String input = "1-2--3--4-5--6--7";
+        TreeNode treeNode = solution.recoverFromPreorder(input);
+        int i= 1;
     }
 }
