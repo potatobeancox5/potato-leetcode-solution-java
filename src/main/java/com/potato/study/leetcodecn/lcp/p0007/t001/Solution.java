dcaded24e3ac800a5b0912b3e9838a07245630d8
liuzhao11 <liuzhao11@sankuai.com>
2021-07-31 22:36:06 +0800

    2021.7.31 LCP 07. 传递信息 2022.10.22

---
+++ src/main/java/com/potato/study/leetcodecn/lcp/p0007/t001/Solution.java
@@ -0,0 +1,106 @@
+package com.potato.study.leetcodecn.lcp.p0007.t001;
+
+import org.junit.Assert;
+
+import java.util.*;
+
+/**
+ * LCP 07. 传递信息
+ *
+ * 小朋友 A 在和 ta 的小伙伴们玩传信息游戏，游戏规则如下：
+
+ 有 n 名玩家，所有玩家编号分别为 0 ～ n-1，其中小朋友 A 的编号为 0
+ 每个玩家都有固定的若干个可传信息的其他玩家（也可能没有）。传信息的关系是单向的（比如 A 可以向 B 传信息，但 B 不能向 A 传信息）。
+ 每轮信息必须需要传递给另一个人，且信息可重复经过同一个人
+ 给定总玩家数 n，以及按 [玩家编号,对应可传递玩家编号] 关系组成的二维数组 relation。返回信息从小 A (编号 0 ) 经过 k 轮传递到编号为 n-1 的小伙伴处的方案数；若不能到达，返回 0。
+
+ 示例 1：
+
+ 输入：n = 5, relation = [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], k = 3
+
+ 输出：3
+
+ 解释：信息从小 A 编号 0 处开始，经 3 轮传递，到达编号 4。共有 3 种方案，分别是 0->2->0->4， 0->2->1->4， 0->2->3->4。
+
+ 示例 2：
+
+ 输入：n = 3, relation = [[0,2],[2,1]], k = 2
+
+ 输出：0
+
+ 解释：信息不能从小 A 处经过 2 轮传递到编号 2
+
+ 限制：
+
+ 2 <= n <= 10
+ 1 <= k <= 5
+ 1 <= relation.length <= 90, 且 relation[i].length == 2
+ 0 <= relation[i][0],relation[i][1] < n 且 relation[i][0] != relation[i][1]
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/chuan-di-xin-xi
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     *
+     * @param n
+     * @param relation
+     * @param k
+     * @return
+     */
+    public int numWays(int n, int[][] relation, int k) {
+        // 遍历 relation 用map 存起来
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] relationship : relation) {
+            List<Integer> list = map.getOrDefault(relationship[0], new ArrayList<>());
+            list.add(relationship[1]);
+            map.put(relationship[0], list);
+        }
+        // 层序遍历 记录 判断能够 在第k轮到达
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        int ways = 0;
+        int layer = 0;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Integer poll = queue.poll();
+                if (layer == k && poll == n-1) {
+                    ways++;
+                }
+                List<Integer> list = map.get(poll);
+                if (null != list) {
+                    queue.addAll(list);
+                }
+            }
+            layer++;
+            if (layer > k) {
+                break;
+            }
+        }
+        return ways;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 5;
+        int[][] relation = new int[][] {
+                {0,2},
+                {2,1},
+                {3,4},
+                {2,3},
+                {1,4},
+                {2,0},
+                {0,4}
+        };
+        int k = 3;
+        int i = solution.numWays(n, relation, k);
+        System.out.println(i);
+        Assert.assertEquals(3, i);
+    }
+
+}
