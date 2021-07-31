73b9fc4041400572566b7ed0d43a927c66dc6970
liuzhao11 <liuzhao11@sankuai.com>
2021-07-31 15:52:18 +0800

    2021.7.31 面试题 02.01. 移除重复节点 2022.10.15

---
+++ src/main/java/com/potato/study/leetcodecn/Interview/p0002/p0001/Solution.java
@@ -0,0 +1,57 @@
+package com.potato.study.leetcodecn.Interview.p0002.p0001;
+
+
+import com.potato.study.leetcode.domain.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 面试题 02.01. 移除重复节点
+ *
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+
+ 示例1:
+
+ 输入：[1, 2, 3, 3, 2, 1]
+ 输出：[1, 2, 3]
+ 示例2:
+
+ 输入：[1, 1, 1, 1, 2]
+ 输出：[1, 2]
+ 提示：
+
+ 链表长度在[0, 20000]范围内。
+ 链表元素在[0, 20000]范围内。
+ 进阶：
+
+ 如果不得使用临时缓冲区，该怎么解决？
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/remove-duplicate-node-lcci
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+
+    public ListNode removeDuplicateNodes(ListNode head) {
+        if (null == head) {
+            return head;
+        }
+        Set<Integer> set = new HashSet<>();
+        ListNode p = head;
+        while (p != null) {
+            set.add(p.val);
+            ListNode q = p.next;
+            while (q != null && set.contains(q.val)) {
+                q = q.next;
+            }
+            // 断链
+            p.next = q;
+            p = p.next;
+        }
+        return head;
+    }
+}
